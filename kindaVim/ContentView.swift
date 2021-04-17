ffcbc78d3855cb178116f254f9179ac6c6d7b2a3
Guill Lo <guill.bout@gmail.com>
2021-04-17 13:26:46 +0800

    create Xcode project

---
+++ kindaVim/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  kindaVim
+//
+//  Created by Guillaume Leclerc on 17/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
