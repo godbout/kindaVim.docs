31c8677f2cca1efc6b5ba2414ef70c748335d3d3
Guill Lo <guill.bout@gmail.com>
2021-05-15 14:10:51 +0800

    add great start for AccessibilityElementAdaptor with tests

--- kindaVim/AccessibilityElement/AccessibilityElementAdaptor.swift
+++ kindaVim/AccessibilityElement/AccessibilityElementAdaptor.swift
@@ -1,13 +1,56 @@
 import Foundation
 
 struct AccessibilityElementAdaptor {
+    
+    // TO THINK:
+    // tried to keep a static variable to the last axFocusedElement
+    // to avoid querying for it again when we write to it.
+    // it makes sense while using the app because we will always
+    // write to the field we currently are in. but it makes testing
+    // harder to understand, as you need to reset the static variable in between.
+    // not sure how to handle this yet. without a static variable, we have to requery,
+    // which seems actually very fast. so maybe it's safer this way.
 
-    func toAccessibilityElement(from axUIElement: AXUIElement) -> AccessibilityElement? {
-        return nil
+    static func fromAXFocusedElement() -> AccessibilityElement? {
+        var accessibilityElement: AccessibilityElement?
+
+        if let axFocusedElement = axFocusedElement() {
+            var values: CFArray?
+            let error = AXUIElementCopyMultipleAttributeValues(axFocusedElement, [kAXValueAttribute, kAXSelectedTextRangeAttribute] as CFArray, .stopOnError, &values)
+
+            if error == .success, let values = values as NSArray? {
+                let text = values[0] as! String
+
+                var selectedTextRange = CFRange()
+                AXValueGetValue(values[1] as! AXValue, .cfRange, &selectedTextRange)
+                let caretLocation = selectedTextRange.location
+
+                accessibilityElement = AccessibilityElement(text: text, caretLocation: caretLocation)
+            }
+        }
+
+        return accessibilityElement
     }
 
-    func toAXUIElememt(from accessibilityElement: AccessibilityElement) -> Bool {
-        return false
+    private static func axFocusedElement() -> AXUIElement? {
+        let axSystemWideElement = AXUIElementCreateSystemWide()
+
+        var axFocusedElement: AnyObject?
+        guard AXUIElementCopyAttributeValue(axSystemWideElement, kAXFocusedUIElementAttribute as CFString, &axFocusedElement) == .success else { return nil }
+
+        return axFocusedElement as! AXUIElement?
     }
 
+    static func toAXFocusedElememt(from accessibilityElement: AccessibilityElement) -> Bool {
+        guard let axFocusedElement = axFocusedElement() else { return false }
+        
+        var selectedTextRange = CFRange()
+        selectedTextRange.location = accessibilityElement.caretLocation
+
+        let newValue = AXValueCreate(.cfRange, &selectedTextRange)
+
+        guard AXUIElementSetAttributeValue(axFocusedElement, kAXSelectedTextRangeAttribute as CFString, newValue!) == .success else { return false }
+        
+        return true
+    }
 }
--- kindaVim/Strategies/AccessibilityStrategy.swift
+++ kindaVim/Strategies/AccessibilityStrategy.swift
@@ -4,13 +4,11 @@ protocol AccessibilityStrategyProtocol {
     
     func h(on element: AccessibilityElement?) -> AccessibilityElement?
     func l(on element: AccessibilityElement?) -> AccessibilityElement?
-    
+
 }
 
 struct AccessibilityStrategy: AccessibilityStrategyProtocol {
-    
-    static var currentAxFocusedElement: AXUIElement?
-    
+
     func h(on element: AccessibilityElement?) -> AccessibilityElement? {
         guard var element = element else { return nil }
         
@@ -26,50 +24,15 @@ struct AccessibilityStrategy: AccessibilityStrategyProtocol {
         
         return element
     }
-    
+
     static func focusedElement() -> AccessibilityElement? {
-        var accessibilityElement: AccessibilityElement?
-        let axSystemWideElement = AXUIElementCreateSystemWide()
-        
-        var axFocusedElement: AnyObject?
-        var error = AXUIElementCopyAttributeValue(axSystemWideElement, kAXFocusedUIElementAttribute as CFString, &axFocusedElement)
-        
-        if error == .success {
-            currentAxFocusedElement = (axFocusedElement as! AXUIElement)
-            
-            var value: AnyObject?
-            error = AXUIElementCopyAttributeValue(axFocusedElement as! AXUIElement, kAXSelectedTextRangeAttribute as CFString, &value)
-    
-            if error == .success {
-                var range = CFRange()
-            
-                if (AXValueGetValue(value as! AXValue, AXValueType.cfRange, &range)) {
-                    accessibilityElement = AccessibilityElement(
-                        caretLocation: range.location
-                    )
-                }
-            }
-            
-        }
-            
-        return accessibilityElement
+        return AccessibilityElementAdaptor.fromAXFocusedElement()
     }
     
     static func write(element: AccessibilityElement) -> Bool {
         print("move using Accessibility Stragety")
 
-        var range = CFRange(
-            location: element.caretLocation,
-            length: 0
-        )
-                                
-        let newValue = AXValueCreate(AXValueType.cfRange, &range)
-        
-        guard let focusedElement = currentAxFocusedElement else { return false }
-    
-        AXUIElementSetAttributeValue(focusedElement, kAXSelectedTextRangeAttribute as CFString, newValue!)
-        
-        return true
+        return AccessibilityElementAdaptor.toAXFocusedElememt(from: element)
     }
     
 }
