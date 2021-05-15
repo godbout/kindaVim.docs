c11da564c141a4fe2bdffb453d9434287df7861f
Guill Lo <guill.bout@gmail.com>
2021-05-15 23:37:49 +0800

    add TextView in testing window and add related Adaptor tests

--- kindaVim/ContentView.swift
+++ kindaVim/ContentView.swift
@@ -1,16 +1,38 @@
 import SwiftUI
 
+struct TextView: NSViewRepresentable {
+
+    @Binding var text: String
+    typealias NSViewType = NSTextView
+
+    func makeNSView(context: Context) -> NSTextView {
+        NSTextView()
+    }
+
+    func updateNSView(_ nsView: NSTextView, context: Context) {
+        nsView.string = text
+    }
+
+}
+
 struct ContentView: View {
+
     @State var textFieldValue = ""
+    @State var textViewValue = ""
 
     var body: some View {
         VStack {
             TextField("single line text field for test", text: $textFieldValue)
                 .padding()
+            TextView(text: $textViewValue)
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                .padding()
+
             Button("wo'hevah", action: {})
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
+
 }
 
 
--- kindaVimUITests/AccessibilityElement/AccessibilityElementAdaptorTests.swift
+++ kindaVimUITests/AccessibilityElement/AccessibilityElementAdaptorTests.swift
@@ -17,32 +17,54 @@ class AccessibilityElementAdaptorTests: XCTestCase {
 extension AccessibilityElementAdaptorTests {
 
     func test_that_it_can_convert_an_AccessibilityElement_to_an_AXUIElement_text_field() {
-        let textTyped = "hello you dear"
         let accessibilityElement = AccessibilityElement(
-            text: textTyped,
+            internalText: "",
             caretLocation: 3
         )
 
+        // can't set to caret location 3 if no text in AXUIElement
+        let textInAXFocusedElement = "hello you dear"
         app.textFields.firstMatch.tap()
-        app.textFields.firstMatch.typeText(textTyped)
+        app.textFields.firstMatch.typeText(textInAXFocusedElement)
 
         let conversionSucceeded = AccessibilityElementAdaptor.toAXFocusedElememt(from: accessibilityElement)
-
         XCTAssertTrue(conversionSucceeded)
 
         let reconvertedAccessibilityElement = AccessibilityElementAdaptor.fromAXFocusedElement()
-
-        XCTAssertEqual(reconvertedAccessibilityElement?.text, textTyped)
+        XCTAssertEqual(reconvertedAccessibilityElement?.internalText, textInAXFocusedElement)
         XCTAssertEqual(reconvertedAccessibilityElement?.caretLocation, 3)
     }
 
     func test_that_it_can_convert_an_AccessibilityElement_to_an_AXUIElement_text_view() {
+        let accessibilityElement = AccessibilityElement(
+            internalText: "",
+            caretLocation: 1
+        )
 
+        // same as above. to be able to set the caret location we need the text in AXUIElement
+        // the internalText of AccessibilityElement is for search purposes, not to set to
+        // the focused AXUIElement (because slow and flickers). we only put the caret location,
+        // and selected text. the text itself comes from what's already in the AXUIElement
+        let textInAXFocusedElement = """
+        so this is some multiline
+            text that i'm trying to
+            type and
+                see if it works.
+        """
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+
+        let conversionSucceeded = AccessibilityElementAdaptor.toAXFocusedElememt(from: accessibilityElement)
+        XCTAssertTrue(conversionSucceeded)
+
+        let reconvertedAccessibilityElement = AccessibilityElementAdaptor.fromAXFocusedElement()
+        XCTAssertEqual(reconvertedAccessibilityElement?.internalText, textInAXFocusedElement)
+        XCTAssertEqual(reconvertedAccessibilityElement?.caretLocation, 1)
     }
 
     func test_that_trying_to_convert_an_AccessibilityElement_to_an_AXUIElement_button_returns_false() {
         let accessibilityElement = AccessibilityElement(
-            text: "hi i'm a button",
+            internalText: "hi i'm a button",
             caretLocation: 5
         )
 
@@ -59,39 +81,33 @@ extension AccessibilityElementAdaptorTests {
 extension AccessibilityElementAdaptorTests {
 
     func test_that_it_can_convert_an_AXUIElement_text_field_to_an_AccessibilityElement() {
-        let textTyped = "some text we will set in some fake UIElement"
-
+        let textInAXFocusedElement = "some text set in some UIElement"
         app.textFields.firstMatch.tap()
-        app.textFields.firstMatch.typeText(textTyped)
+        app.textFields.firstMatch.typeText(textInAXFocusedElement)
         for _ in 1...5 {
             app.textFields.firstMatch.typeKey(.leftArrow, modifierFlags: [])
         }
 
         let accessibilityElement = AccessibilityElementAdaptor.fromAXFocusedElement()
 
-        XCTAssertEqual(accessibilityElement?.text, textTyped)
-        XCTAssertEqual(accessibilityElement?.caretLocation, textTyped.count - 5)
+        XCTAssertEqual(accessibilityElement?.internalText, textInAXFocusedElement)
+        XCTAssertEqual(accessibilityElement?.caretLocation, textInAXFocusedElement.count - 5)
     }
 
     func test_that_it_can_convert_an_AXUIElement_text_view_to_an_AccessibilityElement() {
-        let textTyped =
-            """
-            well that's supposed to be a text view
-            so like you can type many lines
-                and it's like super cool.
-            """
-
+        let textInAXFocusedElement = """
+        well that's supposed to be a text view
+        so like you can type many lines
+          and it's like super cool.
+        """
         app.textViews.firstMatch.tap()
-        app.textViews.firstMatch.typeText(textTyped)
-        app.textViews.firstMatch.typeKey(.upArrow, modifierFlags: [])
-        for _ in 1...7 {
-            app.textFields.firstMatch.typeKey(.leftArrow, modifierFlags: [])
-        }
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        app.textFields.firstMatch.typeKey(.leftArrow, modifierFlags: [])
 
         let accessibilityElement = AccessibilityElementAdaptor.fromAXFocusedElement()
 
-        XCTAssertEqual(accessibilityElement?.text, textTyped)
-        XCTAssertEqual(accessibilityElement?.caretLocation, 45)
+        XCTAssertEqual(accessibilityElement?.internalText, textInAXFocusedElement)
+        XCTAssertEqual(accessibilityElement?.caretLocation, textInAXFocusedElement.count - 1)
     }
 
     func test_that_trying_to_convert_an_AXUIElement_button_to_an_AccessibilityElement_returns_nil() {
