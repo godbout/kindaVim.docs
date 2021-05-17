1484a8d7190b8640fe40a7032845eaccd587cf38
Guill Lo <guill.bout@gmail.com>
2021-05-17 13:29:24 +0800

    add failing AS h and l tests, now it's time to find a proper way to solve

--- kindaVimTests/Unit/Strategies/AccessibilityStrategy/AS_h_Tests.swift
+++ kindaVimTests/Unit/Strategies/AccessibilityStrategy/AS_h_Tests.swift
@@ -3,16 +3,32 @@ import XCTest
 
 class AS_h_Tests: AS_BaseTests {
     
-    func test_that_h_is_moving_cursor_position_to_the_left_by_one_increment() {
+    func test_that_in_normal_setting_h_is_moving_the_caret_position_to_the_left_by_one_increment() {
         let element = AccessibilityElement(
             internalText: "hello world",
             caretLocation: 6
         )
-        
+    
         let returnedElement = accessibilityStrategy.h(on: element)
-        
+    
         XCTAssertEqual(returnedElement?.internalText, element.internalText)
         XCTAssertEqual(returnedElement?.caretLocation, 5)
+    }
+    
+    func test_that_at_the_beginning_of_a_line_h_does_not_move() {
+        let element = AccessibilityElement(
+            internalText: """
+            indeed
+            that is
+            multiline
+            """,
+            caretLocation: 7
+        )
+    
+        let returnedElement = accessibilityStrategy.h(on: element)
+    
+        XCTAssertEqual(returnedElement?.internalText, element.internalText)
+        XCTAssertEqual(returnedElement?.caretLocation, 7)
     }
     
 }
--- kindaVimTests/Unit/Strategies/AccessibilityStrategy/AS_l_Tests.swift
+++ kindaVimTests/Unit/Strategies/AccessibilityStrategy/AS_l_Tests.swift
@@ -3,7 +3,7 @@ import XCTest
 
 class AS_l_Tests: AS_BaseTests {
     
-    func test_that_l_is_moving_cursor_position_to_the_right_by_one_increment() {
+    func test_that_in_normal_setting_l_is_moving_cursor_position_to_the_right_by_one_increment() {
         let element = AccessibilityElement(
             internalText: "hello world",
             caretLocation: 6
@@ -14,6 +14,37 @@ class AS_l_Tests: AS_BaseTests {
         XCTAssertEqual(returnedElement?.internalText, element.internalText)
         XCTAssertEqual(returnedElement?.caretLocation, 7)
     }
-    
+
+    func test_that_l_does_not_move_further_and_caret_position_is_correct_when_line_ends_with_visible_character() {
+        let element = AccessibilityElement(
+            internalText: """
+            indeed
+            that is
+            multiline
+            """,
+            caretLocation: 23
+        )
+
+        let returnedElement = accessibilityStrategy.l(on: element)
+
+        XCTAssertEqual(returnedElement?.internalText, element.internalText)
+        XCTAssertEqual(returnedElement?.caretLocation, 23)
+    }
+
+    func test_that_l_does_not_move_further_and_caret_position_is_correct_when_line_ends_with_invisible_linefeed() {
+        let element = AccessibilityElement(
+            internalText: """
+            indeed
+            that is
+            multiline
+            """,
+            caretLocation: 13
+        )
+
+        let returnedElement = accessibilityStrategy.l(on: element)
+
+        XCTAssertEqual(returnedElement?.internalText, element.internalText)
+        XCTAssertEqual(returnedElement?.caretLocation, 13)
+    }
+
 }
-    
