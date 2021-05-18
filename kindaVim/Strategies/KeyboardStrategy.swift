e9d2fdf887660b9dc267be03ef96e277fae5f2e1
Guill Lo <guill.bout@gmail.com>
2021-05-18 22:38:20 +0800

    fix KS I that didn't work if caret was before first non blank character

--- kindaVim/Strategies/KeyboardStrategy.swift
+++ kindaVim/Strategies/KeyboardStrategy.swift
@@ -200,6 +200,7 @@ struct KeyboardStrategy: KeyboardStrategyProtocol {
 
     func I() -> [KeyCombination] {
         return [
+            KeyCombination(key: .right, command: true),
             KeyCombination(key: .left, command: true)
         ]
     }
--- kindaVimTests/Unit/Strategies/KeyboardStrategy/KS_I__Tests.swift
+++ kindaVimTests/Unit/Strategies/KeyboardStrategy/KS_I__Tests.swift
@@ -6,8 +6,9 @@ class KS_I__Tests: KS_BaseTests {
     func test_that_I_returns_control_a() {
         let keyCombinations = keyboardStrategy.I()
 
-        guard keyCombinations.count == 1 else { return XCTFail() }
-        XCTAssertEqual(keyCombinations[0], KeyCombination(key: .left, command: true))
+        guard keyCombinations.count == 2 else { return XCTFail() }
+        XCTAssertEqual(keyCombinations[0], KeyCombination(key: .right, command: true))
+        XCTAssertEqual(keyCombinations[1], KeyCombination(key: .left, command: true))
     }
     
 }
