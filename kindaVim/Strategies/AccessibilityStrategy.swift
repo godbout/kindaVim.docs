c941062536f481a43f64a71e3134cefddf71dc83
Guill Lo <guill.bout@gmail.com>
2021-05-21 20:31:25 +0800

    remove AS implementation. start from scratch!

--- kindaVim/Strategies/AccessibilityStrategy.swift
+++ kindaVim/Strategies/AccessibilityStrategy.swift
@@ -20,27 +20,11 @@ struct AccessibilityStrategy: AccessibilityStrategyProtocol {
     func h(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
         guard var element = element else { return nil }
 
-        if element.caretLocation > element.lineStart {
-            element.caretLocation -= 1
-        }
-        
         return element
     }
     
     func l(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
         guard var element = element else { return nil }
-
-        // TODO
-        // this will need to be refactored.
-        // TextEngine... engine?
-        let lineStart = element.internalText.index(element.internalText.startIndex, offsetBy: element.lineStart)
-        let lineEnd = element.internalText.index(lineStart, offsetBy: element.lineEnd - element.lineStart)
-        
-        let limit = element.internalText[lineStart..<lineEnd].hasSuffix("\n") ? element.lineEnd - 2 : element.lineEnd - 1
-
-        if element.caretLocation < limit {
-            element.caretLocation += 1
-        }
 
         return element
     }
@@ -48,20 +32,11 @@ struct AccessibilityStrategy: AccessibilityStrategyProtocol {
     func dollarSign(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
         guard var element = element else { return nil }
 
-        let lineStart = element.internalText.index(element.internalText.startIndex, offsetBy: element.lineStart)
-        let lineEnd = element.internalText.index(lineStart, offsetBy: element.lineEnd - element.lineStart)
-
-        let limit = element.internalText[lineStart..<lineEnd].hasSuffix("\n") ? element.lineEnd - 2 : element.lineEnd - 1
-
-        element.caretLocation = limit
-
         return element
     }
 
     func zero(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
         guard var element = element else { return nil }
-
-        element.caretLocation = element.lineStart
 
         return element
     }
@@ -72,7 +47,7 @@ struct AccessibilityStrategy: AccessibilityStrategyProtocol {
         switch status {
         case .on:
             element = h(on: element)!
-            element.selectedLength = 1
+            element.selectedLength = 0
         case .off:
             element.selectedLength = 0
         }
