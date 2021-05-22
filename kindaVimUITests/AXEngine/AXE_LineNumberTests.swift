07c85223b76731a92efc8f227d9c7f58f26dce13
Guill Lo <guill.bout@gmail.com>
2021-05-22 13:47:26 +0800

    add complementary tests for axLineNumber. now all tested

--- kindaVimUITests/AXEngine/AXE_LineNumberTests.swift
+++ kindaVimUITests/AXEngine/AXE_LineNumberTests.swift
@@ -7,7 +7,7 @@ class AXE_LineNumberTests: AXE_BaseTests {}
 extension AXE_LineNumberTests {
 
     func test_that_in_normal_setting_it_can_get_the_AX_line_number() {
-        let textInAXFocusedElement = "that's the text that is in the TextField..."
+        let textInAXFocusedElement = "getting ax line number with caret in between"
         app.textFields.firstMatch.tap()
         app.textFields.firstMatch.typeText(textInAXFocusedElement)
 
@@ -17,13 +17,33 @@ extension AXE_LineNumberTests {
     }
 
     func test_that_it_can_get_the_AX_line_number_when_the_caret_is_at_the_beginning_of_a_line() {
-        let textInAXFocusedElement = "that's the text that is in the TextField... again..."
+        let textInAXFocusedElement = "getting ax line number with caret at beginning"
         app.textFields.firstMatch.tap()
         app.textFields.firstMatch.typeText(textInAXFocusedElement)
 
         let axLineNumber = AXEngine.axLineNumberFor(location: 0)
 
         XCTAssertEqual(axLineNumber, 0)
+    }
+    
+    func test_that_it_cannot_get_the_AX_line_number_when_the_caret_is_at_the_end_of_a_line() {
+        let textInAXFocusedElement = "getting ax line number with caret at end"
+        app.textFields.firstMatch.tap()
+        app.textFields.firstMatch.typeText(textInAXFocusedElement)
+
+        let axLineNumber = AXEngine.axLineNumberFor(location: textInAXFocusedElement.count)
+
+        XCTAssertNil(axLineNumber)
+    }
+    
+    func test_that_it_cannot_get_the_AX_line_number_for_an_empty_TextField() {
+        let textInAXFocusedElement = ""
+        app.textFields.firstMatch.tap()
+        app.textFields.firstMatch.typeText(textInAXFocusedElement)
+
+        let axLineNumber = AXEngine.axLineNumberFor(location: textInAXFocusedElement.count)
+
+        XCTAssertNil(axLineNumber)
     }
 
 }
@@ -71,7 +91,9 @@ that starts to be quite a lot
 
         let axLineNumber = AXEngine.axLineNumberFor(location: 33)
 
-        XCTAssertEqual(axLineNumber, 1)
+        // even if visually on second line, linefeed belongs to previous line
+        // and also axLines start at 0, not 1
+        XCTAssertEqual(axLineNumber, 0)
     }
 
     func test_that_it_can_get_the_correct_AX_line_number_when_the_caret_is_at_the_end_of_a_line() {
@@ -88,6 +110,32 @@ that's big
         let axLineNumber = AXEngine.axLineNumberFor(location: 71)
 
         XCTAssertEqual(axLineNumber, 1)
+    }
+    
+    func test_that_it_cannot_get_the_correct_AX_line_number_when_the_caret_is_at_the_end_of_a_TextView() {
+        let textInAXFocusedElement = """
+some text to put in the TextView
+OMG can you guys speed up the UI Tests
+please please please again again again
+fucking hell
+that's big bis
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+
+        let axLineNumber = AXEngine.axLineNumberFor(location: textInAXFocusedElement.count)
+
+        XCTAssertNil(axLineNumber)
+    }
+    
+    func test_that_it_cannot_get_the_AX_line_number_for_an_empty_TextView() {
+        let textInAXFocusedElement = ""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+
+        let axLineNumber = AXEngine.axLineNumberFor(location: textInAXFocusedElement.count)
+
+        XCTAssertNil(axLineNumber)
     }
 
 }
