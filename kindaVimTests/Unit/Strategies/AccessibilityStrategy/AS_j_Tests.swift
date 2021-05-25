0c0adc3fd8376cfe5cc18a7e919c97b17f741ab5
Guill Lo <guill.bout@gmail.com>
2021-05-26 01:48:15 +0800

    add test for j in normal setting. implementation coming later. big changes

--- kindaVimTests/Unit/Strategies/AccessibilityStrategy/AS_j_Tests.swift
+++ kindaVimTests/Unit/Strategies/AccessibilityStrategy/AS_j_Tests.swift
@@ -21,3 +21,27 @@ extension AS_j_Tests {
     }
     
 }
+
+// TextViews
+extension AS_j_Tests {
+    
+    func test_that_in_normal_setting_j_goes_to_the_next_line_at_the_same_column() {
+        let text = """
+let the fun
+begin with the
+crazy line and
+column shit
+"""
+        
+        let element = AccessibilityTextElement(
+            axValue: text,
+            axCaretLocation: 16,
+            axLineStart: 12,
+            axLineEnd: 27
+        )
+        
+        let returnedElement = accessibilityStrategy.j(on: element)
+        
+        XCTAssertEqual(returnedElement?.axCaretLocation, 31)
+    }
+}
