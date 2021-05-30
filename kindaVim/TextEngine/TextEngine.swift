a297e2638ce5d283a57acb6cde5065cae160861e
Guill Lo <guill.bout@gmail.com>
2021-05-31 02:33:06 +0800

    add Character extension for TextEngine

--- kindaVim/TextEngine/TextEngine.swift
+++ kindaVim/TextEngine/TextEngine.swift
@@ -6,6 +6,23 @@ protocol TextEngineProtocol {
 }
 
 
+extension Character {
+    
+    func isCharacterThatConstitutesAVimWord() -> Bool {
+        return self.isLetter || self.isNumber || self == "_"
+    }
+    
+    func isPunctuationButNotUnderscore() -> Bool {
+        return self.isPunctuation && self != "_"
+    }
+    
+    func isWhitespaceButNotNewline() -> Bool { 
+        return self.isWhitespace && !self.isNewline
+    }
+    
+}
+
+
 struct TextEngine: TextEngineProtocol {
     
     func wordBackward(for location: Int, playground text: String) -> Int {
@@ -16,42 +33,30 @@ struct TextEngine: TextEngineProtocol {
             guard index != startIndex else { return 0 }
             let previousIndex = text.index(before: index)
             
-            if text[index] == "_" {
-                if text[previousIndex].isLetter || text[previousIndex].isNumber || text[previousIndex] == "_" {
+            if text[index].isCharacterThatConstitutesAVimWord() {
+                if text[previousIndex].isCharacterThatConstitutesAVimWord() {
                     continue
                 }
             }
             
-            if text[index].isLetter {
-                if text[previousIndex].isLetter || text[previousIndex].isNumber || text[previousIndex] == "_" {
-                    continue
-                }
-            }
-            
-            if text[index].isNumber {
-                if text[previousIndex].isLetter || text[previousIndex].isNumber || text[previousIndex] == "_" {
-                    continue
-                }
-            }
-            
-            if text[index].isNewline {
-                if !text[previousIndex].isNewline {
-                    continue
-                }
-            }
-                        
-            if text[index].isWhitespace && !text[index].isNewline {
-                continue
-            }
-            
-            if text[index].isPunctuation && text[index] != "_" {
-                if text[previousIndex].isPunctuation && text[previousIndex] != "_" {
+            if text[index].isPunctuationButNotUnderscore() {
+                if text[previousIndex].isPunctuationButNotUnderscore() {
                     continue
                 }
             }
             
             if text[index].isSymbol {
-                if text[previousIndex].isSymbol || (text[previousIndex].isPunctuation && text[previousIndex] != "_") {
+                if text[previousIndex].isSymbol || text[previousIndex].isPunctuationButNotUnderscore() {
+                    continue
+                }
+            }
+            
+            if text[index].isWhitespaceButNotNewline() {
+                continue
+            }
+            
+            if text[index].isNewline {
+                if !text[previousIndex].isNewline {
                     continue
                 }
             }
@@ -70,44 +75,32 @@ struct TextEngine: TextEngineProtocol {
             guard index != text.index(before: endIndex) else { return text.count - 1 }
             let nextIndex = text.index(after: index)
             
-            if text[index] == "_" {
-                if text[nextIndex].isLetter || text[nextIndex].isNumber || text[nextIndex] == "_" || text[nextIndex].isWhitespace {
+            if text[index].isCharacterThatConstitutesAVimWord() {
+                if text[nextIndex].isCharacterThatConstitutesAVimWord() || text[nextIndex].isWhitespace {
                     continue
                 }
             }
                         
-            if text[index].isPunctuation && text[index] != "_" {
-                if (text[nextIndex].isPunctuation && text[nextIndex] != "_") || text[nextIndex].isSymbol || text[nextIndex].isWhitespace {
-                    continue
-                }
-            }
-            
-            if text[index].isLetter {
-                if text[nextIndex].isLetter || text[nextIndex].isNumber || text[nextIndex] == "_" || text[nextIndex].isWhitespace {
-                    continue
-                }
-            }
-            
-            if text[index].isNumber {
-                if text[nextIndex].isLetter || text[nextIndex].isNumber || text[nextIndex] == "_" || text[nextIndex].isWhitespace {
-                    continue
-                }
-            }
-            
-            if text[index].isWhitespace && !text[index].isNewline {
-                if text[nextIndex].isWhitespace {
-                    continue
-                }
-            }
-            
-            if text[index].isNewline {
-                if text[nextIndex].isWhitespace && !text[nextIndex].isNewline {
+            if text[index].isPunctuationButNotUnderscore() {
+                if text[nextIndex].isPunctuationButNotUnderscore() || text[nextIndex].isSymbol || text[nextIndex].isWhitespace {
                     continue
                 }
             }
             
             if text[index].isSymbol {
                 if text[nextIndex].isSymbol || text[nextIndex].isWhitespace {
+                    continue
+                }
+            }
+                        
+            if text[index].isWhitespaceButNotNewline() {
+                if text[nextIndex].isWhitespace {
+                    continue
+                }
+            }
+            
+            if text[index].isNewline {
+                if text[nextIndex].isWhitespaceButNotNewline() {
                     continue
                 }
             }
