eababa506540f71b4cfe7d0382e3f8da16332f6a
Guill Lo <guill.bout@gmail.com>
2021-06-01 16:09:03 +0800

    add VimEngine tests for f

---
+++ UnitTests/VimEngine/UsingAS/UsingAS_f_Tests.swift
@@ -0,0 +1,24 @@
+@testable import kindaVim
+import XCTest
+
+class UsingAS_f_Tests: UsingAS_BaseTests {
+    
+    override func setUp() {
+        super.setUp()
+        
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .f))
+    }
+    
+}
+
+extension UsingAS_f_Tests {
+    
+    func test_that_it_calls_the_correct_function_on_accessibility_strategy() {
+        XCTAssertEqual(accessibilityStrategyMock.functionCalled, "f(characterToGoTo:on:)")
+    }
+    
+    func test_that_it_keeps_Vim_in_normal_mode() {
+        XCTAssertEqual(VimEngine.shared.currentMode, .normal)
+    }
+    
+}
