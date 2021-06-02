2b28c4c164935397c54627b002483d90365c1003
Guill Lo <guill.bout@gmail.com>
2021-06-03 01:06:33 +0800

    remove useless test of VE current mode for ci" in AS. moved to UI Tests

---
+++ AccessibilityStrategyUITests/UIAS_ci"_Tests.swift
@@ -0,0 +1,8 @@
+//
+//  UIAS_ci"_Tests.swift
+//  AccessibilityStrategyUITests
+//
+//  Created by Guillaume Leclerc on 02/06/2021.
+//
+
+import Foundation
--- UnitTests/VimEngine/UsingAS/UsingAS_ci"_Tests.swift
+++ UnitTests/VimEngine/UsingAS/UsingAS_ci"_Tests.swift
@@ -13,14 +13,14 @@ class UsingAS_ciDoubleQuote_Tests: UsingAS_BaseTests {
     
 }
 
+// we do not have the test to see if ci" switches into insert mode here
+// and the reason is because it depends.
+// if ci" can find quotes, it will go into insert mode
+// else it will stay into normal mode. so this has to be tested somewhere else
 extension UsingAS_ciDoubleQuote_Tests {
     
     func test_that_ciDoubleQuote_calls_the_ciDoubleQuote_function_on_accessibility_strategy() {
         XCTAssertEqual(accessibilityStrategyMock.functionCalled, "ciDoubleQuote(on:)")
     }
-    
-    func test_that_ciDoubleQuote_switches_Vim_into_insert_mode() {
-        XCTAssertEqual(VimEngine.shared.currentMode, .insert)
-    }
-    
+        
 }
