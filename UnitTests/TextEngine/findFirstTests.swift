727690c201ae9afd897a8fc246b1eeaba45835c0
Guill Lo <guill.bout@gmail.com>
2021-06-04 00:54:05 +0800

    add TE findFirst tests

--- UnitTests/TextEngine/findFirstTests.swift
+++ UnitTests/TextEngine/findFirstTests.swift
@@ -7,15 +7,23 @@ class findFirstTests: TextEngineBaseTests {}
 extension findFirstTests {
     
     func test_that_in_normal_setting_it_returns_the_correct_location() {
-        let text = """
-some text with at least one " in it
-"""
-        let location = textEngine.findFirst("\"", in: text)
+        let text = "find a z where there is one"
+        let location = textEngine.findFirst("z", in: text)
         
-        XCTAssertEqual(location, 28)        
+        XCTAssertEqual(location, 7)        
     }
     
-    
+    func test_that_it_returns_nil_if_it_cannot_find_the_character() {
+        let text = """
+in this one
+there is no
+well that letter
+that we cannot say
+"""
+        let location = textEngine.findFirst("z", in: text)
+        
+        XCTAssertNil(location)
+    }
     
 }
 
@@ -23,5 +31,15 @@ some text with at least one " in it
 // TextViews
 extension findFirstTests {
     
+    func test_that_it_does_return_the_first_of_a_whole_multiline_not_just_first_of_the_line() {
+        let text = """
+there's a z here
+but also one here z
+and even z here!
+"""
+        let location = textEngine.findFirst("z", in: text)
+        
+        XCTAssertEqual(location, 10) 
+    }
     
 }
