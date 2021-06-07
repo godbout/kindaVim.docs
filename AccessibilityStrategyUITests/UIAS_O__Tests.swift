ee9bb0ae63379b0f5664818f553490be1e945429
Guill Lo <guill.bout@gmail.com>
2021-06-08 02:23:01 +0800

    move O tests to real UI Tests

--- AccessibilityStrategyUITests/UIAS_O__Tests.swift
+++ AccessibilityStrategyUITests/UIAS_O__Tests.swift
@@ -2,7 +2,17 @@
 import XCTest
 
 
-class UIAS_O__Tests: UIAS_BaseTests {}
+class UIAS_O__Tests: UIAS_BaseTests {
+    
+    private func applyMoveAndGetBackUpdatedElement() -> AccessibilityTextElement? {
+        let accessibilityElement = AccessibilityTextElementAdaptor.fromAXFocusedElement()
+        guard let transformedElement = accessibilityStrategy.O(on: accessibilityElement) else { return nil }
+        _ = AccessibilityTextElementAdaptor.toAXfocusedElement(from: transformedElement)
+        
+        return  AccessibilityTextElementAdaptor.fromAXFocusedElement()        
+    }
+    
+}
 
 
 // TextViews
@@ -15,17 +25,22 @@ and O will
 create a new line
 above!
 """
-        
         app.textViews.firstMatch.tap()
         app.textViews.firstMatch.typeText(textInAXFocusedElement)
         app.textViews.firstMatch.typeKey(.upArrow, modifierFlags: [])
         app.textViews.firstMatch.typeKey(.upArrow, modifierFlags: [])
                 
-        let accessibilityElement = accessibilityStrategy.O(on: AccessibilityTextElementAdaptor.fromAXFocusedElement())
+        let finalElement = applyMoveAndGetBackUpdatedElement()        
         
-        XCTAssertEqual(accessibilityElement?.caretLocation, 0)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 18)
-        XCTAssertEqual(accessibilityElement?.selectedText, "that's a multiline\n")  
+        XCTAssertEqual(finalElement?.value, """
+that's a multiline
+
+and O will
+create a new line
+above!
+"""
+        )
+        XCTAssertEqual(finalElement?.caretLocation, 19)
     }
     
     func test_that_if_at_the_first_line_it_will_create_a_new_line_above() {
@@ -39,14 +54,16 @@ still create a line above
         app.textViews.firstMatch.typeKey(.upArrow, modifierFlags: [])
         app.textViews.firstMatch.typeKey(.upArrow, modifierFlags: [])
         
-        let accessibilityElement = accessibilityStrategy.O(on: AccessibilityTextElementAdaptor.fromAXFocusedElement())
+        let finalElement = applyMoveAndGetBackUpdatedElement()        
         
-        XCTAssertEqual(accessibilityElement?.caretLocation, 0)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
-        // first a linefeed is sent to AXFocusedElement, then nothing in order to reposition 
-        // the caret. it's dumb but haven't found a bette way for now. setting the selectedText
-        // always moves the caret right after. can't find a way with AX API to add text after the caret without moving it
-        XCTAssertEqual(accessibilityElement?.selectedText, "")  
+        XCTAssertEqual(finalElement?.value, """
+
+caret on the first
+line and it should
+still create a line above
+"""
+        )
+        XCTAssertEqual(finalElement?.caretLocation, 0)
     }
     
     func test_that_if_on_an_empty_line_it_will_still_create_a_line_above() {
@@ -59,11 +76,16 @@ an empty line
         app.textViews.firstMatch.typeText(textInAXFocusedElement)
         app.textViews.firstMatch.typeKey(.upArrow, modifierFlags: [])
         
-        let accessibilityElement = accessibilityStrategy.O(on: AccessibilityTextElementAdaptor.fromAXFocusedElement())
+        let finalElement = applyMoveAndGetBackUpdatedElement()        
         
-        XCTAssertEqual(accessibilityElement?.caretLocation, 0)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 12)
-        XCTAssertEqual(accessibilityElement?.selectedText, "there is now\n")  
+        XCTAssertEqual(finalElement?.value, """
+there is now
+
+
+an empty line
+"""
+        )
+        XCTAssertEqual(finalElement?.caretLocation, 13)
     }
     
     func test_that_if_on_the_last_line_it_creates_a_line_below_and_the_caret_stays_on_the_current_line() {
@@ -76,14 +98,17 @@ the last empty line
         app.textViews.firstMatch.tap()
         app.textViews.firstMatch.typeText(textInAXFocusedElement)
 
-        let accessibilityElement = accessibilityStrategy.O(on: AccessibilityTextElementAdaptor.fromAXFocusedElement())
+        let finalElement = applyMoveAndGetBackUpdatedElement()        
         
-        XCTAssertEqual(accessibilityElement?.caretLocation, 45)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
-        // first a linefeed is sent to AXFocusedElement, then nothing in order to reposition 
-        // the caret. it's dumb but haven't found a bette way for now. setting the selectedText
-        // always moves the caret right after. can't find a way with AX API to add text after the caret without moving it
-        XCTAssertEqual(accessibilityElement?.selectedText, "")  
+        XCTAssertEqual(finalElement?.value, """
+now the caret
+will be on
+the last empty line
+
+
+"""
+        )
+        XCTAssertEqual(finalElement?.caretLocation, 45)
     }
     
     func test_that_it_creates_a_line_above_and_goes_to_the_same_indentation_as_the_current_line() {
@@ -96,11 +121,16 @@ but it should work
         app.textViews.firstMatch.typeText(textInAXFocusedElement)
         app.textViews.firstMatch.typeKey(.upArrow, modifierFlags: [])
         
-        let accessibilityElement = accessibilityStrategy.O(on: AccessibilityTextElementAdaptor.fromAXFocusedElement())
+        let finalElement = applyMoveAndGetBackUpdatedElement()        
         
-        XCTAssertEqual(accessibilityElement?.caretLocation, 0)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 11)
-        XCTAssertEqual(accessibilityElement?.selectedText, "now there's\n    ")  
+        XCTAssertEqual(finalElement?.value, """
+now there's
+    
+    some indent
+but it should work
+"""
+        )
+        XCTAssertEqual(finalElement?.caretLocation, 16)
     }
     
     func test_that_if_keeps_the_indentation_even_if_it_is_on_the_first_line() {
@@ -111,11 +141,14 @@ but it should work
         app.textViews.firstMatch.typeText(textInAXFocusedElement)
         app.textViews.firstMatch.typeKey(.leftArrow, modifierFlags: [])
         
-        let accessibilityElement = accessibilityStrategy.O(on: AccessibilityTextElementAdaptor.fromAXFocusedElement())
+        let finalElement = applyMoveAndGetBackUpdatedElement()        
         
-        XCTAssertEqual(accessibilityElement?.caretLocation, 0)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
-        XCTAssertEqual(accessibilityElement?.selectedText, "   ")  
+        XCTAssertEqual(finalElement?.value, """
+   
+   now indent on the first line
+"""
+        )
+        XCTAssertEqual(finalElement?.caretLocation, 3)
     }
     
 }
