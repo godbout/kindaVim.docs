cfd70dfc0217a909845550eebade1088fdc591a2
Guill Lo <guill.bout@gmail.com>
2021-06-08 17:31:23 +0800

    add AS cc tests and implementation

--- UnitTests/Strategies/AccessibilityStrategy/AS_cc_Tests.swift
+++ UnitTests/Strategies/AccessibilityStrategy/AS_cc_Tests.swift
@@ -1,8 +1,168 @@
-//
-//  AS_cc_Tests.swift
-//  UniTests
-//
-//  Created by Guillaume Leclerc on 08/06/2021.
-//
+@testable import kindaVim
+import XCTest
 
-import Foundation
+
+class AS_cc_Tests: AS_BaseTests {}
+
+
+// the 3 special cases:
+// - empty TextElement
+// - caret at the end of TextElement but not on empty line
+// - caret at the end of TextElement on own empty line
+extension AS_cc_Tests {
+    
+    func test_that_if_the_TextElement_is_empty_it_does_nothing_and_does_not_crash() {
+        let text = ""
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            caretLocation: 0,
+            currentLine: AccessibilityTextElementLine(
+                fullValue: text,
+                number: nil,
+                start: nil,
+                end: nil
+            )
+        )
+        
+        let returnedElement = accessibilityStrategy.cc(on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 0)
+        XCTAssertEqual(returnedElement?.selectedLength, 0)
+        XCTAssertNil(returnedElement?.selectedText)
+    }
+    
+    func test_that_if_the_caret_is_at_the_last_character_of_the_TextElement_but_not_on_an_empty_line_it_does_nothing_and_does_not_crash() {
+        let text = """
+caret is
+gonna be at the end
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            caretLocation: 28,
+            currentLine: AccessibilityTextElementLine(
+                fullValue: text,
+                number: nil,
+                start: nil,
+                end: nil
+            )
+        )
+        
+        let returnedElement = accessibilityStrategy.cc(on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 28)
+        XCTAssertEqual(returnedElement?.selectedLength, 0)
+        XCTAssertNil(returnedElement?.selectedText)
+
+    }
+    
+    func test_that_if_the_caret_is_at_the_last_character_of_the_TextElement_and_on_an_empty_line_on_its_own_it_deletes_the_non_existing_line_lol_so_basically_it_looks_like_it_does_nothing() {
+        let text = """
+caret is on its
+own empty
+line
+
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            caretLocation: 31,
+            currentLine: AccessibilityTextElementLine(
+                fullValue: text,
+                number: nil,
+                start: nil,
+                end: nil
+            )
+        )
+        
+        let returnedElement = accessibilityStrategy.cc(on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 31)
+        XCTAssertEqual(returnedElement?.selectedLength, 0)
+        XCTAssertNil(returnedElement?.selectedText)
+    }
+    
+}
+
+
+// Both
+extension AS_cc_Tests {
+    
+    func test_that_in_normal_setting_it_deletes_the_whole_line() {
+        let text = "this is a single line"
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            caretLocation: 13,
+            currentLine: AccessibilityTextElementLine(
+                fullValue: text,
+                number: 0,
+                start: 0,
+                end: 21
+            )
+        )
+        
+        let returnedElement = accessibilityStrategy.cc(on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 0)
+        XCTAssertEqual(returnedElement?.selectedLength, 21)
+        XCTAssertEqual(returnedElement?.selectedText, "")
+    }
+    
+}
+
+
+// TextViews
+extension AS_cc_Tests {
+    
+    func test_that_in_normal_setting_it_deletes_the_whole_correct_line() {
+        let text = """
+should delete
+the right line
+lol
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            caretLocation: 23,
+            currentLine: AccessibilityTextElementLine(
+                fullValue: text,
+                number: 1,
+                start: 14,
+                end: 29
+            )
+        )
+        
+        let returnedElement = accessibilityStrategy.cc(on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 14)
+        XCTAssertEqual(returnedElement?.selectedLength, 14)
+        XCTAssertEqual(returnedElement?.selectedText, "")
+    }
+    
+    func test_that_it_should_keep_the_indentation_of_the_current_line() {
+        let text = """
+   i delete a line
+but the indent should
+be kept
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            caretLocation: 10,
+            currentLine: AccessibilityTextElementLine(
+                fullValue: text,
+                number: 0,
+                start: 0,
+                end: 19
+            )
+        )
+        
+        let returnedElement = accessibilityStrategy.cc(on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 3)
+        XCTAssertEqual(returnedElement?.selectedLength, 15)
+        XCTAssertEqual(returnedElement?.selectedText, "")
+    }
+}
--- kindaVim/Strategies/AccessibilityStrategy.swift
+++ kindaVim/Strategies/AccessibilityStrategy.swift
@@ -67,6 +67,25 @@ struct AccessibilityStrategy: AccessibilityStrategyProtocol {
     func cc(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
         guard var element = element  else { return nil }
         
+        if element.isEmpty() {
+            return element
+        }
+        
+        if element.caretIsAtTheEnd(), element.lastCharacterIsNotLinefeed() {
+            return element
+        }
+        
+        if element.caretIsAtTheEnd(), element.lastCharacterIsLinefeed() {
+            return element            
+        }
+        
+        let lineText = element.currentLine.value
+        let firstNonBlankOfCurrentLineLocation = textEngine.findFirstNonBlank(in: lineText)
+                
+        element.caretLocation = element.currentLine.start! + firstNonBlankOfCurrentLineLocation
+        element.selectedLength = element.currentLine.lengthWithoutLinefeed! - firstNonBlankOfCurrentLineLocation
+        element.selectedText = ""
+
         return element
     }
     
