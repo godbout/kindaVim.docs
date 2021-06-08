1f29f07a21c97543463b91d265b50d10d2aaf350
Guill Lo <guill.bout@gmail.com>
2021-06-08 18:40:20 +0800

    refactor AS l with the 4 cases

--- kindaVim/Strategies/AccessibilityStrategy.swift
+++ kindaVim/Strategies/AccessibilityStrategy.swift
@@ -361,8 +361,21 @@ struct AccessibilityStrategy: AccessibilityStrategyProtocol {
 
     func l(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
         guard var element = element else { return nil }
+        
+        if element.isEmpty() {
+            return element
+        }
+        
+        if element.caretIsAtTheEnd(), element.lastCharacterIsNotLinefeed() {
+            return element
+        }
+        
+        if element.caretIsAtTheEnd(), element.lastCharacterIsLinefeed() {
+            return element
+        }
 
-        if let endLimit = element.currentLine.endLimit(), element.caretLocation < endLimit {
+        
+        if element.caretLocation < element.currentLine.endLimit()! {
             element.caretLocation += 1
         }
 
