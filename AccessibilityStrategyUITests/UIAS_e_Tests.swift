77305397062289300499066b8df972fc2087d2a1
Guill Lo <guill.bout@gmail.com>
2021-06-08 22:33:24 +0800

    add AS UI Tests for e :))

---
+++ AccessibilityStrategyUITests/UIAS_e_Tests.swift
@@ -0,0 +1,36 @@
+@testable import kindaVim
+import XCTest
+
+
+// these tests might be removed later
+// not sure it's very worthy to create
+// tests to assure the globalColumnNumber
+// is updated properly for each linewise
+// move
+class UIAS_e_Tests: UIAS_BaseTests {}
+
+
+// TextViews
+extension UIAS_e_Tests {
+    
+    func test_that_it_does_update_the_globalColumnNumber_correctly() {
+        let textInAXFocusedElement = """
+this is to make
+sure that when the move
+changes line then
+the globalColumnNumber
+gets updated properly
+"""
+        
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        app.textViews.firstMatch.typeKey(.leftArrow, modifierFlags: [.command])
+        app.textViews.firstMatch.typeKey(.leftArrow, modifierFlags: [])
+        
+        // need to move the caretLocation to have a proper AccessibilityTextElement.globalColumnNumber
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .e))
+        
+        XCTAssertEqual(AccessibilityTextElement.globalColumnNumber, 4)
+    }
+    
+}
