223b19788ca266233b7bef9beb863a8e058c8cf3
Guill Lo <guill.bout@gmail.com>
2021-06-11 02:38:07 +0800

    add final implementation of AS ^, with tests

---
+++ UnitTests/Strategies/AccessibilityStrategy/AS_^_Tests.swift
@@ -0,0 +1,166 @@
+@testable import kindaVim
+import XCTest
+
+
+// the ^ move is using the TextEngine.firstNonBlank function only
+// which is already heavily tested. so there are only a few tests here,
+// some for redundancy, some that are specific to the ^ move, like
+// The 3 Cases, or the fact that it should not stop at the end of
+// the line itself like firstNonBlank, but at the end limit
+class AS_caret_Tests: AS_BaseTests {}
+
+
+// the 3 special cases:
+// - empty TextElement
+// - caret at the end of TextElement but not on empty line
+// - caret at the end of TextElement on own empty line
+extension AS_caret_Tests {
+    
+    func test_that_if_the_TextElement_is_empty_it_does_nothing_and_does_not_crash() {
+        let text = ""
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            caretLocation: 0,
+            currentLine: AccessibilityTextElementLine(
+                fullValue: text,
+                number: nil,
+                start: nil,
+                end: nil
+            )
+        )
+        
+        let returnedElement = accessibilityStrategy.caret(on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 0)
+    }
+    
+    func test_that_if_the_caret_is_at_the_last_character_of_the_TextElement_but_not_on_an_empty_line_it_does_nothing_and_does_not_crash() {
+        let text = """
+caret is
+gonna be at the end
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            caretLocation: 28,
+            currentLine: AccessibilityTextElementLine(
+                fullValue: text,
+                number: nil,
+                start: nil,
+                end: nil
+            )
+        )
+        
+        let returnedElement = accessibilityStrategy.caret(on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 28)        
+    }
+    
+    func test_that_if_the_caret_is_at_the_last_character_of_the_TextElement_and_on_an_empty_line_on_its_own_it_deletes_the_non_existing_line_lol_so_basically_it_looks_like_it_does_nothing() {
+        let text = """
+caret is on its
+own empty
+line
+
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            caretLocation: 31,
+            currentLine: AccessibilityTextElementLine(
+                fullValue: text,
+                number: nil,
+                start: nil,
+                end: nil
+            )
+        )
+        
+        let returnedElement = accessibilityStrategy.caret(on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 31)
+    }    
+    
+}
+
+
+// Both
+extension AS_caret_Tests {
+    
+    func test_that_in_normal_case_it_goes_to_the_first_non_blank_of_the_line() {
+        let text = "    hehe ankulay"
+        
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            caretLocation: 2,
+            currentLine: AccessibilityTextElementLine(
+                fullValue: text,
+                number: 0,
+                start: 0,
+                end: 16
+            )
+        )
+        
+        let returnedElement = accessibilityStrategy.caret(on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 4)        
+    }
+    
+    func test_that_if_the_text_is_just_spaces_and_no_linefeed_then_the_caret_goes_at_the_end_of_the_text() {
+        let text = """
+a multiline
+with a last line
+without a linefeed but with spaces
+                      
+"""
+        
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            caretLocation: 71,
+            currentLine: AccessibilityTextElementLine(
+                fullValue: text,
+                number: 3,
+                start: 64,
+                end: 86
+            )
+        )
+        
+        let returnedElement = accessibilityStrategy.caret(on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 85)    
+    }
+    
+}
+
+
+// TextViews 
+extension AS_caret_Tests {
+    
+    func test_that_for_spaces_and_a_linefeed_it_stops_before_the_linefeed_at_the_correct_end_limit() {
+        let text = """
+this time the
+empty line has a linefeed
+                      
+ yes
+"""
+        
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            caretLocation: 44,
+            currentLine: AccessibilityTextElementLine(
+                fullValue: text,
+                number: 2,
+                start: 40,
+                end: 63
+            )
+        )
+        
+        let returnedElement = accessibilityStrategy.caret(on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 61)   
+    }
+    
+}
--- kindaVim/Strategies/AccessibilityStrategy.swift
+++ kindaVim/Strategies/AccessibilityStrategy.swift
@@ -578,6 +578,29 @@ struct AccessibilityStrategy: AccessibilityStrategyProtocol {
     func caret(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
         guard var element = element else { return nil }
         
+        if element.isEmpty {
+            return element
+        }
+        
+        if element.caretIsAtTheEnd, element.lastCharacterIsNotLinefeed {
+            return element
+        }
+        
+        if element.caretIsAtTheEnd, element.lastCharacterIsLinefeed {
+            return element
+        }
+        
+        
+        let lineText = element.currentLine.value        
+        let characterFoundLocation = textEngine.firstNonBlank(in: lineText)
+        let newCaretLocation = element.currentLine.start! + characterFoundLocation
+                
+        if newCaretLocation >= element.currentLine.endLimit! { 
+            element.caretLocation = element.currentLine.endLimit!
+        } else {
+            element.caretLocation = newCaretLocation
+        }         
+        
         return element
     }
 
