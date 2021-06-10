fe06e87b958b305cff64fdf9c357fa22c73f7346
Guill Lo <guill.bout@gmail.com>
2021-06-10 23:18:09 +0800

    add tests for TE innerWord

---
+++ UnitTests/TextEngine/VimMoves/innerWordTests.swift
@@ -0,0 +1,93 @@
+@testable import kindaVim
+import XCTest
+
+
+// here we don't need much tests for what is a Vim word
+// as this is already tested in other Text Engine funcs.
+// innerWord is using beginningOfWordBackward and endOfWordForward
+// for its calculation (which are already tested), except for whitespaces
+// which is why it's more important to have the whitespaces tested here
+class innerWordTests: TextEngineBaseTests {}
+
+
+// Both
+extension innerWordTests {
+    
+    func test_that_it_the_caret_is_on_a_letter_if_finds_the_correct_inner_word() {
+        let text = "ok we're gonna try to get the inner word here"
+        
+        let (wordStartLocation, wordEndLocsation) = textEngine.innerWord(startingAt: 10, in: text)
+        
+        XCTAssertEqual(wordStartLocation, 9)
+        XCTAssertEqual(wordEndLocsation, 14) 
+    }
+    
+    func test_that_if_the_caret_is_on_a_space_the_inner_word_is_all_the_consecutive_spaces() {
+        let text = "ok so now we have a lot of     spaces"
+        
+        let (wordStartLocation, wordEndLocsation) = textEngine.innerWord(startingAt: 28, in: text)
+        
+        XCTAssertEqual(wordStartLocation, 26)
+        XCTAssertEqual(wordEndLocsation, 31)         
+    }
+    
+    func test_that_if_the_caret_is_on_a_single_space_it_recognizes_it_as_an_inner_word() {
+        let text = "a single space is an inner word"
+        
+        let (wordStartLocation, wordEndLocsation) = textEngine.innerWord(startingAt: 20, in: text)
+        
+        XCTAssertEqual(wordStartLocation, 20)
+        XCTAssertEqual(wordEndLocsation, 21) 
+    }
+    
+    func test_that_if_the_TextField_starts_with_spaces_it_finds_the_correct_inner_word() {
+        let text = "     that's lots of spaces"
+        
+        let (wordStartLocation, wordEndLocsation) = textEngine.innerWord(startingAt: 4, in: text)
+        
+        XCTAssertEqual(wordStartLocation, 0)
+        XCTAssertEqual(wordEndLocsation, 5) 
+    }
+    
+    func test_that_if_the_TextField_ends_with_spaces_it_still_gets_the_correct_inner_word() {
+        let text = "that's lots of spaces again       "
+        
+        let (wordStartLocation, wordEndLocsation) = textEngine.innerWord(startingAt: 29, in: text)
+        
+        XCTAssertEqual(wordStartLocation, 27)
+        XCTAssertEqual(wordEndLocsation, 34) 
+    }
+    
+}
+
+
+// TextViews
+extension innerWordTests {
+    
+    func test_that_inner_word_stops_at_linefeeds_at_the_end_of_lines() {
+        let text = """
+this shouldn't
+spill      
+   on the next line
+"""
+        
+        let (wordStartLocation, wordEndLocsation) = textEngine.innerWord(startingAt: 23, in: text)
+        
+        XCTAssertEqual(wordStartLocation, 20)
+        XCTAssertEqual(wordEndLocsation, 26)
+    }
+    
+    func test_that_inner_word_stops_at_linefeeds_at_the_beginning_of_lines() {
+        let text = """
+this shouldn't
+spill also    
+    backwards
+"""
+        
+        let (wordStartLocation, wordEndLocsation) = textEngine.innerWord(startingAt: 33, in: text)
+        
+        XCTAssertEqual(wordStartLocation, 30)
+        XCTAssertEqual(wordEndLocsation, 34)
+    }
+    
+}
