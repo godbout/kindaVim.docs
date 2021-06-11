edafde62ee33d00ca7fa23dc7eff3c42ae75ea4a
Guill Lo <guill.bout@gmail.com>
2021-06-12 00:48:35 +0800

    add AS _ implementation, and tests

--- kindaVim/Strategies/AccessibilityStrategy.swift
+++ kindaVim/Strategies/AccessibilityStrategy.swift
@@ -660,6 +660,29 @@ struct AccessibilityStrategy: AccessibilityStrategyProtocol {
     func underscore(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
         guard var element = element else { return nil }
         
+        if element.isEmpty {
+            return element
+        }
+        
+        if element.caretIsAtTheEnd, element.lastCharacterIsNotLinefeed {
+            return element
+        }
+        
+        if element.caretIsAtTheEnd, element.lastCharacterIsLinefeed {
+            return element
+        }
+        
+        
+        let lineText = element.currentLine.value        
+        let characterFoundLocation = textEngine.firstNonBlank(in: lineText)
+        let newCaretLocation = element.currentLine.start! + characterFoundLocation
+        
+        if newCaretLocation >= element.currentLine.endLimit! { 
+            element.caretLocation = element.currentLine.endLimit!
+        } else {
+            element.caretLocation = newCaretLocation
+        }         
+        
         return element
     }
 
