b334100041446eeb8065d09f8d43c7a3a7c29881
Guill Lo <guill.bout@gmail.com>
2021-06-19 01:54:38 +0800

    add basic simple Unit tests for TE nextUnmatched and previousUnmatched
    the most of the tests are done in findNextUnmatched and findPreviousUnmatched

--- UnitTests/TextEngine/VimMoves/nextUnmatchedTests.swift
+++ UnitTests/TextEngine/VimMoves/nextUnmatchedTests.swift
@@ -1,8 +1,32 @@
-//
-//  nextUnmatchedTests.swift
-//  UnitTests
-//
-//  Created by Guillaume Leclerc on 18/06/2021.
-//
+@testable import kindaVim
+import XCTest
 
-import Foundation
+
+// nextUnmatched is like a wrapper around TE.findNextUnmatched
+// that will return the same location it was give if a next
+// unmatched can't be found (and therefore findNextUnmatched returns
+// nil). here we just test this. the rest of findNextUnmatched
+// is tested in its own place
+class nextUnmatchedTests: TextEngineBaseTests {}
+
+
+// Both
+extension nextUnmatchedTests {
+    
+    func test_that_if_it_cannot_find_an_unmatched_bracket_it_returns_the_location_it_was_given() {
+        let text = "ok we're gonna try to get the inner word here"
+        
+        let location = textEngine.nextUnmatched("}", after: 4, in: text)
+        
+        XCTAssertEqual(location, 4) 
+    }
+    
+    func test_that_in_normal_cases_it_works_hehe() {
+        let text = " a couple of ( (( ))))  ) O_o"
+        
+        let location = textEngine.nextUnmatched(")", after: 15, in: text)
+        
+        XCTAssertEqual(location, 20)
+    }
+    
+}
--- UnitTests/TextEngine/VimMoves/previousUnmatchedTests.swift
+++ UnitTests/TextEngine/VimMoves/previousUnmatchedTests.swift
@@ -1,8 +1,28 @@
-//
-//  previousUnmatchedTests.swift
-//  UnitTests
-//
-//  Created by Guillaume Leclerc on 18/06/2021.
-//
+@testable import kindaVim
+import XCTest
 
-import Foundation
+
+// same blah blah as nextUnmatched
+class previousUnmatchedTests: TextEngineBaseTests {}
+
+
+// Both
+extension previousUnmatchedTests {
+    
+    func test_that_if_it_cannot_find_an_unmatched_bracket_it_returns_the_location_it_was_given() {
+        let text = "ok we're gonna try to get the XXX here"
+        
+        let location = textEngine.previousUnmatched("{", before: 1, in: text)
+        
+        XCTAssertEqual(location, 1) 
+    }
+    
+    func test_that_in_normal_cases_it_works_hehe() {
+        let text = " a couple of ( ( )"
+        
+        let location = textEngine.previousUnmatched("(", before: 15, in: text)
+        
+        XCTAssertEqual(location, 13)
+    }
+    
+}
