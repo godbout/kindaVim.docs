4cd01bc6d672ba73c625afc9a39e8437bacb46a8
Guill Lo <guill.bout@gmail.com>
2021-06-20 17:19:13 +0800

    add missing UI Tests for cT

---
+++ AccessibilityStrategyUITests/UIAS_cT__Tests.swift
@@ -0,0 +1,41 @@
+@testable import kindaVim
+import XCTest
+
+
+class UIAS_cT__Tests: UIAS_BaseTests {}
+
+
+// Both
+extension UIAS_cT__Tests {
+    
+    func test_that_in_normal_setting_it_succeeds_and_switches_to_insert_mode() {
+        let textInAXFocusedElement = """
+cT is pretty cool
+"""
+        app.textFields.firstMatch.tap()
+        app.textFields.firstMatch.typeText(textInAXFocusedElement)
+        app.textFields.firstMatch.typeKey(.leftArrow, modifierFlags: [])
+        
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .c))
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .t, shift: true))
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .p))
+        
+        XCTAssertEqual(VimEngine.shared.currentMode, .insert)        
+    }
+    
+    func test_a_case_where_it_should_not_delete_the_content_stays_in_normal_mode() {
+        let textInAXFocusedElement = """
+hehe can't use cT here with the last letter of the alphabet!
+"""                
+        app.textFields.firstMatch.tap()
+        app.textFields.firstMatch.typeText(textInAXFocusedElement)                
+        app.textFields.firstMatch.typeKey(.leftArrow, modifierFlags: [.option])
+        
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .c))
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .t, shift: true))
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .z))
+        
+        XCTAssertEqual(VimEngine.shared.currentMode, .normal)      
+    }
+    
+}
