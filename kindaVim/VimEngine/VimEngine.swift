ef8ca120a31200ad6651239b1093a1f52b01fcf7
Guill Lo <guill.bout@gmail.com>
2021-06-25 01:27:34 +0800

    small refactor VE VM v for consistency vith V

--- kindaVim/VimEngine/VimEngine.swift
+++ kindaVim/VimEngine/VimEngine.swift
@@ -771,16 +771,16 @@ extension VimEngine {
                 push(element: element)
             }
         case .v:
+            if var element = asVisualMode.v(on: focusedElement()) {
+                element.selectedLength = 1
+                push(element: element)
+            }
+            
             if visualStyle == .characterwise {
                 enterNormalMode()
             } else {
                 visualStyle = .characterwise
             }
-            
-            if var element = asVisualMode.v(on: focusedElement()) {
-                element.selectedLength = 1
-                push(element: element)
-            }            
         case .V:
             if var element = asVisualMode.V(on: focusedElement()) {
                 if visualStyle == .linewise {
