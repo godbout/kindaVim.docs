a5343fe9dc7db804d73da63ab3b841ab1d6fa783
Guill Lo <guill.bout@gmail.com>
2021-06-26 03:39:47 +0800

    add current impl of VM V

--- kindaVim/Strategies/AccessibilityStrategy/VisualMode/ASVM_V_.swift
+++ kindaVim/Strategies/AccessibilityStrategy/VisualMode/ASVM_V_.swift
@@ -3,6 +3,9 @@ extension AccessibilityStrategyVisualMode {
     func V(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
         guard var element = element else { return nil }
         
+        Self.anchor = element.caretLocation
+        Self.head = element.caretLocation
+        
         if element.isEmpty {
             return element
         }
