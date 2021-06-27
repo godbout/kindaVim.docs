49154bde82665f06cf302577eaa6c15fe19d4c4d
Guill Lo <guill.bout@gmail.com>
2021-06-27 20:03:45 +0800

    fix VM d if on last (non empty) line

--- kindaVim/Strategies/AccessibilityStrategy/VisualMode/ASVM_d.swift
+++ kindaVim/Strategies/AccessibilityStrategy/VisualMode/ASVM_d.swift
@@ -30,7 +30,21 @@ extension AccessibilityStrategyVisualMode {
                 element.caretLocation += firstNonBlankWithinLineLimitOfNextLineLocation
                 element.selectedLength = 0
                 element.selectedText = nil
-            }                
+            } else if let axPreviousLine = AXEngine.axLineRangeFor(lineNumber: element.currentLine.number! - 1) {
+                let value = element.value
+                let axPreviousLineText = String(value[value.index(value.startIndex, offsetBy: axPreviousLine.location)..<value.index(value.startIndex, offsetBy: axPreviousLine.location + axPreviousLine.length)])
+                let firstNonBlankWithinLineLimitOfPreviousLineLocation = textEngine.firstNonBlankWithinLineLimit(in: TextEngineLine(from: axPreviousLineText))
+                
+                element.caretLocation = element.currentLine.start! - 1
+                element.selectedLength = element.currentLine.length! + 1
+                element.selectedText = ""
+                
+                _ = AccessibilityTextElementAdaptor.toAXfocusedElement(from: element)
+                
+                element.caretLocation -= axPreviousLineText.count - firstNonBlankWithinLineLimitOfPreviousLineLocation - 1                
+                element.selectedLength = 0
+                element.selectedText = ""
+            }
         }
         
         return element
