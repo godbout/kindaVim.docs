b911d08b7151230fa0920d0ab8de48d6bdd3bd53
Guill Lo <guill.bout@gmail.com>
2021-06-28 17:57:22 +0800

    add first version of VM c

--- AccessibilityStrategyUITests/VisualMode/UIASVM_c_Tests.swift
+++ AccessibilityStrategyUITests/VisualMode/UIASVM_c_Tests.swift
@@ -1,8 +1,41 @@
-//
-//  UIASVM_c_Tests.swift
-//  AccessibilityStrategyUITests
-//
-//  Created by Guillaume Leclerc on 28/06/2021.
-//
+import XCTest
 
-import Foundation
+
+class UIASVM_c_Tests: UIAS_BaseTests {
+    
+    private func applyMovesAndGetBackUpdatedElement() -> AccessibilityTextElement? {
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .v, shift: true))
+        
+        return applyMoveAndGetBackUpdatedElement { focusedElement in
+            asVisualMode.c(on: focusedElement)
+        }
+    }
+    
+}
+
+
+// linewise
+extension UIASVM_c_Tests {
+    
+    func test_that_the_line_gets_deleted_until_the_linefeed_and_the_caret_ends_up_at_the_beginning_of_that_line() {    
+        let textInAXFocusedElement = """
+we gonna use VM
+d here and we suppose
+      to go to non blank of the line
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        app.textViews.firstMatch.typeKey(.upArrow, modifierFlags: [])  
+        
+        let finalElement = applyMovesAndGetBackUpdatedElement()
+        
+        XCTAssertEqual(finalElement?.value, """
+we gonna use VM
+
+      to go to non blank of the line
+"""
+        )
+        XCTAssertEqual(finalElement?.caretLocation, 16)        
+    }
+        
+}
--- kindaVim/Strategies/AccessibilityStrategy/VisualMode/ASVM_c.swift
+++ kindaVim/Strategies/AccessibilityStrategy/VisualMode/ASVM_c.swift
@@ -16,36 +16,13 @@ extension AccessibilityStrategyVisualMode {
         }
         
         
-        element.selectedText = ""
-        
-        
-//        if VimEngine.shared.visualStyle == .linewise {
-//            if let axNextLine = AXEngine.axLineRangeFor(lineNumber: element.currentLine.number! + 1) {
-//                let value = element.value
-//                let axNextLineText = String(value[value.index(value.startIndex, offsetBy: axNextLine.location)..<value.index(value.startIndex, offsetBy: axNextLine.location + axNextLine.length)])
-//                let firstNonBlankWithinLineLimitOfNextLineLocation = textEngine.firstNonBlankWithinLineLimit(in: TextEngineLine(from: axNextLineText))
-//                
-//                _ = AccessibilityTextElementAdaptor.toAXfocusedElement(from: element)
-//                
-//                element.caretLocation += firstNonBlankWithinLineLimitOfNextLineLocation
-//                element.selectedLength = 0
-//                element.selectedText = nil
-//            } else if let axPreviousLine = AXEngine.axLineRangeFor(lineNumber: element.currentLine.number! - 1) {
-//                let value = element.value
-//                let axPreviousLineText = String(value[value.index(value.startIndex, offsetBy: axPreviousLine.location)..<value.index(value.startIndex, offsetBy: axPreviousLine.location + axPreviousLine.length)])
-//                let firstNonBlankWithinLineLimitOfPreviousLineLocation = textEngine.firstNonBlankWithinLineLimit(in: TextEngineLine(from: axPreviousLineText))
-//                
-//                element.caretLocation = element.currentLine.start! - 1
-//                element.selectedLength = element.currentLine.length! + 1
-//                element.selectedText = ""
-//                
-//                _ = AccessibilityTextElementAdaptor.toAXfocusedElement(from: element)
-//                
-//                element.caretLocation -= axPreviousLineText.count - firstNonBlankWithinLineLimitOfPreviousLineLocation - 1                
-//                element.selectedLength = 0
-//                element.selectedText = ""
-//            }
-//        }
+        if VimEngine.shared.visualStyle == .linewise {
+            element.caretLocation = element.currentLine.start!
+            element.selectedLength = element.currentLine.lengthWithoutLinefeed!
+            element.selectedText = ""
+        } else {
+            element.selectedText = ""
+        }
         
         return element
     }
