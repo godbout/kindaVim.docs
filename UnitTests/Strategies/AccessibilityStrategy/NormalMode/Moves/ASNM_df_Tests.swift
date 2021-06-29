a0e3d8a47f54339a7d9efa31a1e1910d2613241a
Guill Lo <guill.bout@gmail.com>
2021-06-29 15:16:32 +0800

    update df after ATEAdaptor improvement

--- UnitTests/Strategies/AccessibilityStrategy/NormalMode/Moves/ASNM_df_Tests.swift
+++ UnitTests/Strategies/AccessibilityStrategy/NormalMode/Moves/ASNM_df_Tests.swift
@@ -16,80 +16,80 @@ class ASNM_df_Tests: ASNM_BaseTests {
 // - empty TextElement
 // - caret at the end of TextElement but not on empty line
 // - caret at the end of TextElement on own empty line
-//extension ASNM_df_Tests {
-//    
-//    func test_that_if_the_TextElement_is_empty_it_does_nothing_and_does_not_crash() {
-//        let text = ""
-//        let element = AccessibilityTextElement(
-//            role: .textField,
-//            value: text,
-//            caretLocation: 0,
-//            currentLine: AccessibilityTextElementLine(
-//                fullValue: text,
-//                number: nil,
-//                start: nil,
-//                end: nil
-//            )
-//        )
-//        
-//        let returnedElement = applyMove(to: "a", on: element)
-//        
-//        XCTAssertEqual(returnedElement?.caretLocation, 0)
-//        XCTAssertEqual(returnedElement?.selectedLength, 0)
-//        XCTAssertNil(returnedElement?.selectedText)
-//    }
-//    
-//    func test_that_if_the_caret_is_at_the_last_character_of_the_TextElement_but_not_on_an_empty_line_it_does_nothing_and_does_not_crash() {
-//        let text = """
-//caret is
-//gonna be at the end
-//"""
-//        let element = AccessibilityTextElement(
-//            role: .textArea,
-//            value: text,
-//            caretLocation: 28,
-//            currentLine: AccessibilityTextElementLine(
-//                fullValue: text,
-//                number: nil,
-//                start: nil,
-//                end: nil
-//            )
-//        )
-//        
-//        let returnedElement = applyMove(to: "b", on: element)
-//        
-//        XCTAssertEqual(returnedElement?.caretLocation, 28)
-//        XCTAssertEqual(returnedElement?.selectedLength, 0)
-//        XCTAssertNil(returnedElement?.selectedText)
-//    }
-//    
-//    func test_that_if_the_caret_is_at_the_last_character_of_the_TextElement_and_on_an_empty_line_on_its_own_it_does_nothing_and_does_not_crash() {
-//        let text = """
-//caret is on its
-//own empty
-//line
-//
-//"""
-//        let element = AccessibilityTextElement(
-//            role: .textArea,
-//            value: text,
-//            caretLocation: 31,
-//            currentLine: AccessibilityTextElementLine(
-//                fullValue: text,
-//                number: nil,
-//                start: nil,
-//                end: nil
-//            )
-//        )
-//        
-//        let returnedElement = applyMove(to: "c", on: element)
-//        
-//        XCTAssertEqual(returnedElement?.caretLocation, 31)
-//        XCTAssertEqual(returnedElement?.selectedLength, 0)
-//        XCTAssertNil(returnedElement?.selectedText)
-//    }    
-//    
-//}
+extension ASNM_df_Tests {
+    
+    func test_that_if_the_TextElement_is_empty_it_does_nothing() {
+        let text = ""
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            caretLocation: 0,
+            currentLine: AccessibilityTextElementLine(
+                fullValue: text,
+                number: 1,
+                start: 0,
+                end: 0
+            )
+        )
+        
+        let returnedElement = applyMove(to: "a", on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 0)
+        XCTAssertEqual(returnedElement?.selectedLength, 0)
+        XCTAssertNil(returnedElement?.selectedText)
+    }
+    
+    func test_that_if_the_caret_is_at_the_last_character_of_the_TextElement_but_not_on_an_empty_line_it_does_nothing() {
+        let text = """
+caret is
+gonna be at the end
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            caretLocation: 28,
+            currentLine: AccessibilityTextElementLine(
+                fullValue: text,
+                number: 2,
+                start: 9,
+                end: 28
+            )
+        )
+        
+        let returnedElement = applyMove(to: "b", on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 28)
+        XCTAssertEqual(returnedElement?.selectedLength, 0)
+        XCTAssertNil(returnedElement?.selectedText)
+    }
+    
+    func test_that_if_the_caret_is_at_the_last_character_of_the_TextElement_and_on_an_empty_line_on_its_own_it_does_nothing() {
+        let text = """
+caret is on its
+own empty
+line
+
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            caretLocation: 31,
+            currentLine: AccessibilityTextElementLine(
+                fullValue: text,
+                number: 4,
+                start: 31,
+                end: 31
+            )
+        )
+        
+        let returnedElement = applyMove(to: "c", on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 31)
+        XCTAssertEqual(returnedElement?.selectedLength, 0)
+        XCTAssertNil(returnedElement?.selectedText)
+    }
+    
+}
 
 
 // Both
--- kindaVim/Strategies/AccessibilityStrategy/NormalMode/ASNM_df.swift
+++ kindaVim/Strategies/AccessibilityStrategy/NormalMode/ASNM_df.swift
@@ -3,19 +3,6 @@ extension AccessibilityStrategyNormalMode {
     func df(to character: Character, on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
         guard var element = element else { return nil }
         
-        if element.isEmpty {
-            return element
-        }
-        
-        if element.caretIsAtTheEnd, element.lastCharacterIsNotLinefeed {
-            return element
-        }
-        
-        if element.caretIsAtTheEnd, element.lastCharacterIsLinefeed {
-            return element
-        }
-        
-        
         if let elementFound = f(to: character, on: element), element != elementFound {
             element.selectedLength = (elementFound.caretLocation + 1) - element.caretLocation
             element.selectedText = ""
