9009f31e2c7caf8ca94575e449ed20bd9bbbe1cc
Guill Lo <guill.bout@gmail.com>
2021-06-29 15:09:28 +0800

    update NM cc after ATEAdaptor improvement

--- kindaVim/Strategies/AccessibilityStrategy/NormalMode/ASNM_cc.swift
+++ kindaVim/Strategies/AccessibilityStrategy/NormalMode/ASNM_cc.swift
@@ -3,17 +3,17 @@ extension AccessibilityStrategyNormalMode {
     func cc(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
         guard var element = element  else { return nil }
         
-        if element.isEmpty {
-            return element
-        }
-        
-        if element.caretIsAtTheEnd, element.lastCharacterIsNotLinefeed {
-            return element
-        }
-        
-        if element.caretIsAtTheEnd, element.lastCharacterIsLinefeed {
-            return element            
-        }
+//        if element.isEmpty {
+//            return element
+//        }
+//        
+//        if element.caretIsAtTheEnd, element.lastCharacterIsNotLinefeed {
+//            return element
+//        }
+//        
+//        if element.caretIsAtTheEnd, element.lastCharacterIsLinefeed {
+//            return element            
+//        }
         
         
         let lineText = element.currentLine.value
