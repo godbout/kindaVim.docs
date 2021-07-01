9dbdefc589703aa53daf9061ef67f25861cfd0e9
Guill Lo <guill.bout@gmail.com>
2021-07-02 02:58:17 +0800

    enhance NM j by making sure selectedLength is 0 and text nil

--- kindaVim/Strategies/AccessibilityStrategy/NormalMode/ASNM_j.swift
+++ kindaVim/Strategies/AccessibilityStrategy/NormalMode/ASNM_j.swift
@@ -4,6 +4,10 @@ extension AccessibilityStrategyNormalMode {
         guard var element = element else { return nil }
         guard element.role == .textArea else { return nil }
         
+        element.selectedLength = 0
+        element.selectedText = nil
+        
+        
         if element.isEmpty {
             return element
         }
