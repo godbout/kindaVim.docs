535179e0852377355b214be56f3855f49b898dc6
Guill Lo <guill.bout@gmail.com>
2021-07-06 00:52:36 +0800

    move Unit Tests for VM escape to UI Tests as we need lineAtHead

---
+++ AccessibilityStrategyUITests/VisualMode/Linewise/UIASVML_escape_Tests.swift
@@ -0,0 +1,73 @@
+import XCTest
+
+
+class UIASVML_escape_Tests: UIAS_BaseTests {}
+
+
+// currently same as Characterwise, so not bothering
+// the 3 special cases:
+// - empty TextElement
+// - caret at the end of TextElement but not on empty line
+// - caret at the end of TextElement on own empty line
+extension UIASVML_escape_Tests {}
+
+
+// TextFields
+extension UIASVML_escape_Tests {
+    
+    func test_that_the_caret_location_goes_to_the_head() {
+        let textInAXFocusedElement = "some plain simple text for once"
+        app.textFields.firstMatch.tap()
+        app.textFields.firstMatch.typeText(textInAXFocusedElement)
+        VimEngine.shared.enterNormalMode()
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .v, shift: true))        
+        
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .escape))
+        let accessibilityElement = AccessibilityTextElementAdaptor.fromAXFocusedElement()
+        
+        XCTAssertEqual(accessibilityElement?.caretLocation, 30)
+    }
+    
+}
+
+
+// TextViews
+extension UIASVML_escape_Tests {
+    
+    func test_that_the_caret_location_goes_to_the_head_even_when_the_selection_spans_over_multiple_lines() {
+        let textInAXFocusedElement = """
+let's try with selecting
+over multiple lines
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        VimEngine.shared.enterNormalMode()        
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .v, shift: true))        
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .k))
+        
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .escape))
+        let accessibilityElement = AccessibilityTextElementAdaptor.fromAXFocusedElement()
+        
+        XCTAssertEqual(accessibilityElement?.caretLocation, 0)
+    }
+    
+    func test_that_if_the_head_is_above_line_end_limit_then_the_caret_goes_to_the_end_limit() {
+        let textInAXFocusedElement = """
+so this is definitely
+gonna go after
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        app.textViews.firstMatch.typeKey(.upArrow, modifierFlags: [])
+        VimEngine.shared.enterNormalMode()
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .v, shift: true))
+        // can be replaced by other moves, or even $ when fixed, later
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .j))
+        
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .escape))
+        let accessibilityElement = AccessibilityTextElementAdaptor.fromAXFocusedElement()
+        
+        XCTAssertEqual(accessibilityElement?.caretLocation, 35)
+    }    
+    
+}
--- UnitTests/Strategies/AccessibilityStrategy/VisualMode/Moves/Linewise/ASVML_escape_Tests.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ASVML_escape_Tests.swift
-//  UnitTests
-//
-//  Created by Guillaume Leclerc on 05/07/2021.
-//
-
-import Foundation
