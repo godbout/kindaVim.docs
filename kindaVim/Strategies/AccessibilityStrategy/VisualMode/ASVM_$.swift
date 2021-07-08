c75548c6067bd71acdc91aa13c047d1220864348
Guill Lo <guill.bout@gmail.com>
2021-07-08 13:30:17 +0800

    refactor VM $ C

--- kindaVim/Strategies/AccessibilityStrategy/VisualMode/ASVM_$.swift
+++ kindaVim/Strategies/AccessibilityStrategy/VisualMode/ASVM_$.swift
@@ -20,22 +20,15 @@ extension AccessibilityStrategyVisualMode {
         }
         
         
-        if VimEngine.shared.visualStyle == .characterwise {
-            if Self.head >= Self.anchor {
-                if let lineAtHead = AccessibilityTextElementAdaptor.lineFor(location: AccessibilityStrategyVisualMode.head) {
-                    element.selectedLength = lineAtHead.end - element.caretLocation
-                }
-            } else if Self.head < Self.anchor {
-                if let lineAtHead = AccessibilityTextElementAdaptor.lineFor(location: AccessibilityStrategyVisualMode.head) {
-                    if lineAtHead.end >= Self.anchor {
-                        element.caretLocation = Self.anchor
-                        element.selectedLength = lineAtHead.end - element.caretLocation                        
-                    } else if lineAtHead.end < Self.anchor {
-                        element.caretLocation = lineAtHead.end - 1
-                        element.selectedLength = (Self.anchor + 1) - element.caretLocation
-                    }
-                }
+        if VimEngine.shared.visualStyle == .characterwise, let lineAtHead = AccessibilityTextElementAdaptor.lineFor(location: AccessibilityStrategyVisualMode.head) {
+            if Self.head >= Self.anchor || lineAtHead.end >= Self.anchor {
+                element.caretLocation = Self.anchor
+                element.selectedLength = lineAtHead.end - element.caretLocation
+            } else {
+                element.caretLocation = lineAtHead.end - 1
+                element.selectedLength = (Self.anchor + 1) - element.caretLocation
             }
+            
         }
         
         return element
