b62a4a5203806981c16fa58b09945997da9abc04
Guill Lo <guill.bout@gmail.com>
2021-07-09 19:46:44 +0800

    add VM k C impl and UI tests, without global column number yet

--- AccessibilityStrategyUITests/VisualMode/Characterwise/UIASVMC_k_Tests.swift
+++ AccessibilityStrategyUITests/VisualMode/Characterwise/UIASVMC_k_Tests.swift
@@ -2,14 +2,14 @@
 import XCTest
 
 
-class UIASSVMC_j_Tests: UIAS_BaseTests {}
+class UIASSVMC_k_Tests: UIAS_BaseTests {}
 
 
 // the 3 special cases:
 // - empty TextElement
 // - caret at the end of TextElement but not on empty line
 // - caret at the end of TextElement on own empty line
-extension UIASSVMC_j_Tests {
+extension UIASSVMC_k_Tests {
     
     func test_that_if_the_TextElement_is_empty_it_works_and_does_not_move() {
         let textInAXFocusedElement = ""
@@ -18,7 +18,7 @@ extension UIASSVMC_j_Tests {
         VimEngine.shared.enterNormalMode()
         VimEngine.shared.handle(keyCombination: KeyCombination(key: .v))        
         
-        VimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .j))
+        VimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .k))
         let accessibilityElement = AccessibilityTextElementAdaptor.fromAXFocusedElement()
         
         XCTAssertEqual(accessibilityElement?.caretLocation, 0)
@@ -35,7 +35,7 @@ gonna be at the end
         VimEngine.shared.handle(keyCombination: KeyCombination(key: .v))
         app.textViews.firstMatch.typeKey(.rightArrow, modifierFlags: [])
         
-        VimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .j))        
+        VimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .k))        
         let accessibilityElement = AccessibilityTextElementAdaptor.fromAXFocusedElement()
         
         XCTAssertEqual(accessibilityElement?.caretLocation, 30)
@@ -54,7 +54,7 @@ own empty
         VimEngine.shared.handle(keyCombination: KeyCombination(key: .v))
         app.textViews.firstMatch.typeKey(.rightArrow, modifierFlags: [])
         
-        VimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .j))                
+        VimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .k))                
         let accessibilityElement = AccessibilityTextElementAdaptor.fromAXFocusedElement()
         
         XCTAssertEqual(accessibilityElement?.caretLocation, 35)
@@ -64,7 +64,7 @@ own empty
 
 
 // TextFields
-extension UIASSVMC_j_Tests {
+extension UIASSVMC_k_Tests {
     
     func test_that_in_TextFields_it_does_nothing() {
         let textInAXFocusedElement = "VM jk in TextFields will do nothing"
@@ -74,7 +74,7 @@ extension UIASSVMC_j_Tests {
         VimEngine.shared.handle(keyCombination: KeyCombination(key: .b))
         VimEngine.shared.handle(keyCombination: KeyCombination(key: .v))
         
-        VimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .j))                
+        VimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .k))                
         let accessibilityElement = AccessibilityTextElementAdaptor.fromAXFocusedElement()
         
         XCTAssertEqual(accessibilityElement?.caretLocation, 28)
@@ -85,9 +85,28 @@ extension UIASSVMC_j_Tests {
 
 
 // TextViews
-extension UIASSVMC_j_Tests {
+extension UIASSVMC_k_Tests {
     
-    func test_that_if_the_head_is_after_the_anchor_then_it_goes_to_the_line_below_the_head_on_the_same_column_number_and_selects_from_the_anchor_to_that_new_head_location() {
+    func test_that_if_the_head_is_before_the_anchor_then_it_goes_to_the_line_above_the_head_on_the_same_column_number_and_selects_from_that_new_head_location_to_the_anchor() {
+        let textInAXFocusedElement = """
+wow that one is
+gonna rip my
+ass off lol
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        VimEngine.shared.enterNormalMode()
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .k))
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .v))
+        
+        VimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .k))
+        let accessibilityElement = AccessibilityTextElementAdaptor.fromAXFocusedElement()
+        
+        XCTAssertEqual(accessibilityElement?.caretLocation, 10)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 17)
+    }
+    
+    func test_that_if_the_head_is_after_the_anchor_and_both_are_on_the_same_line_then_it_goes_to_the_line_above_the_head_on_the_same_column_number_and_selects_from_that_new_head_location_to_the_anchor() {
         let textInAXFocusedElement = """
 wow that one is
 gonna rip my
@@ -101,14 +120,37 @@ ass off lol
         VimEngine.shared.handle(keyCombination: KeyCombination(key: .v))
         VimEngine.shared.handle(keyCombination: KeyCombination(key: .e))
         
-        VimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .j))                
+        VimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .k))                
+        let accessibilityElement = AccessibilityTextElementAdaptor.fromAXFocusedElement()
+        
+        XCTAssertEqual(accessibilityElement?.caretLocation, 4)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 13)
+    }    
+    
+    func test_that_if_the_head_is_after_the_anchor_and_both_are_not_on_the_same_line_and_the_new_head_location_is_after_the_anchor_then_it_goes_to_the_line_above_the_head_on_the_same_column_number_and_selects_from_the_anchor_to_that_new_head_location() {
+        let textInAXFocusedElement = """
+wow that one is
+gonna rip my
+ass off lol
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        VimEngine.shared.enterNormalMode()
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .k))
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .zero))
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .v))
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .e))
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .j))
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .e))
+        
+        VimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .k))                
         let accessibilityElement = AccessibilityTextElementAdaptor.fromAXFocusedElement()
         
         XCTAssertEqual(accessibilityElement?.caretLocation, 16)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 18)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 7)        
     }
     
-    func test_that_if_the_head_is_before_the_anchor_and_both_are_on_the_same_line_then_it_goes_to_the_line_below_the_head_on_the_same_column_number_and_selects_from_the_anchor_to_that_new_head_location() {
+    func test_that_if_the_head_is_after_the_anchor_and_both_are_not_on_the_same_line_and_the_new_head_location_is_before_the_anchor_then_it_goes_to_the_line_above_the_head_on_the_same_column_number_and_selects_from_that_new_head_location_to_the_anchor() {
         let textInAXFocusedElement = """
 wow that one is
 gonna rip my
@@ -117,84 +159,16 @@ ass off lol
         app.textViews.firstMatch.tap()
         app.textViews.firstMatch.typeText(textInAXFocusedElement)
         VimEngine.shared.enterNormalMode()
-        VimEngine.shared.handle(keyCombination: KeyCombination(key: .k))
-        VimEngine.shared.handle(keyCombination: KeyCombination(key: .v))
-        VimEngine.shared.handle(keyCombination: KeyCombination(key: .b))
-        
-        VimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .j))                
-        let accessibilityElement = AccessibilityTextElementAdaptor.fromAXFocusedElement()
-        
-        XCTAssertEqual(accessibilityElement?.caretLocation, 26)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 10)
-    }    
-    
-    func test_that_if_the_head_is_before_the_anchor_and_both_are_not_on_the_same_line_and_the_new_head_location_is_before_the_anchor_then_it_goes_to_the_line_below_the_head_on_the_same_column_number_and_selects_from_that_new_head_location_to_the_anchor() {
-        let textInAXFocusedElement = """
-wow that one is
-gonna rip my
-ass off lol
-"""
-        app.textViews.firstMatch.tap()
-        app.textViews.firstMatch.typeText(textInAXFocusedElement)
-        VimEngine.shared.enterNormalMode()
-        VimEngine.shared.handle(keyCombination: KeyCombination(key: .k))
-        VimEngine.shared.handle(keyCombination: KeyCombination(key: .v))
-        // to refactor when once we have implemented better moves
-        VimEngine.shared.handle(keyCombination: KeyCombination(key: .zero))
-        VimEngine.shared.handle(keyCombination: KeyCombination(key: .b))
-        VimEngine.shared.handle(keyCombination: KeyCombination(key: .b))
-        
-        VimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .j))                
-        let accessibilityElement = AccessibilityTextElementAdaptor.fromAXFocusedElement()
-        
-        XCTAssertEqual(accessibilityElement?.caretLocation, 25)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 2)        
-    }
-        
-    func test_that_if_the_line_below_the_head_line_is_shorter_then_it_goes_to_the_end_of_that_line_and_does_not_spill_over_the_next_next_line() {
-        let textInAXFocusedElement = """
-wow that one is
-gonna rip my
-ass off lol
-"""
-        app.textViews.firstMatch.tap()
-        app.textViews.firstMatch.typeText(textInAXFocusedElement)
-        VimEngine.shared.enterNormalMode()
-        VimEngine.shared.handle(keyCombination: KeyCombination(key: .k))
         VimEngine.shared.handle(keyCombination: KeyCombination(key: .k))
         VimEngine.shared.handle(keyCombination: KeyCombination(key: .v))
         VimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .dollarSign))
+        VimEngine.shared.handle(keyCombination: KeyCombination(key: .e))
         
-        VimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .j))                
+        VimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .k))                
         let accessibilityElement = AccessibilityTextElementAdaptor.fromAXFocusedElement()
         
-        XCTAssertEqual(accessibilityElement?.caretLocation, 10)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 19)                
-    }
-    
-    // currently not fully compatible with Vim as moves like $ should bring all the following lines
-    // to the end of line too, but currently we keep track of a precise column number rather than "the end of a line"
-    func test_that_it_keeps_track_of_the_column_number() {
-        let textInAXFocusedElement = """
-wow that one is
-gonna rip my
-ass off lol
-extra long one here
-"""
-        app.textViews.firstMatch.tap()
-        app.textViews.firstMatch.typeText(textInAXFocusedElement)
-        app.textViews.firstMatch.typeKey(.upArrow, modifierFlags: [.command])
-        VimEngine.shared.enterNormalMode()
-        VimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .dollarSign))
-        VimEngine.shared.handle(keyCombination: KeyCombination(key: .v))
-        VimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .j))
-        VimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .j))
-        
-        VimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .j))                
-        let accessibilityElement = AccessibilityTextElementAdaptor.fromAXFocusedElement()
-        
-        XCTAssertEqual(accessibilityElement?.caretLocation, 14)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 42)           
+        XCTAssertEqual(accessibilityElement?.caretLocation, 18)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 9)        
     }
     
 }
--- kindaVim/Strategies/AccessibilityStrategy/VisualMode/ASVM_k.swift
+++ kindaVim/Strategies/AccessibilityStrategy/VisualMode/ASVM_k.swift
@@ -3,9 +3,56 @@ extension AccessibilityStrategyVisualMode {
     func k(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
         guard let element = element else { return nil }
         
+        if VimEngine.shared.visualStyle == .characterwise {
+            return kForVisualModeCharacterwise(on: element)
+        }
+        
         if VimEngine.shared.visualStyle == .linewise {
             return kForVisualModeLinewise(on: element)
         }
+        
+        return element
+    }
+    
+    private func kForVisualModeCharacterwise(on element: AccessibilityTextElement) -> AccessibilityTextElement {
+        var element = element
+        
+        
+        if element.isEmpty {
+            return element
+        }
+        
+        if element.caretIsAtTheEnd, element.lastCharacterIsNotLinefeed {
+            element.caretLocation -= 1
+            element.selectedLength = 1
+            
+            return element
+        }
+        
+        if element.caretIsAtTheEnd, element.lastCharacterIsLinefeed {
+            return element
+        }
+        
+        
+        if let lineAtHead = AccessibilityTextElementAdaptor.lineFor(location: Self.head), let lineAboveHead = AccessibilityTextElementAdaptor.lineFor(location: lineAtHead.start - 1) {
+            var newHeadLocation = lineAboveHead.start + (AccessibilityTextElement.globalColumnNumber - 1)
+            var globalColumnNumber: Int?
+            
+            // if the new head is over the line limit, we set it at the line limit,
+            // and we will override the setting of the globalColumnNumber
+            if newHeadLocation >= lineAboveHead.end {
+                newHeadLocation = lineAboveHead.end - 1
+                globalColumnNumber = AccessibilityTextElement.globalColumnNumber
+            }
+            
+            if Self.head > Self.anchor, newHeadLocation > Self.anchor {
+                element.selectedLength = (newHeadLocation - Self.anchor) + 1
+            } else {
+                element.caretLocation = newHeadLocation
+                element.selectedLength = (Self.anchor - newHeadLocation) + 1
+            }
+        }
+        
         
         return element
     }
