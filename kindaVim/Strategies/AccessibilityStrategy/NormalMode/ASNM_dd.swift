fb50cb259e252489f893bd4acae51c7695aee6c5
Guill Lo <guill.bout@gmail.com>
2021-07-13 20:19:42 +0800

    refactor dd and dj

--- kindaVim/Strategies/AccessibilityStrategy/NormalMode/ASNM_dd.swift
+++ kindaVim/Strategies/AccessibilityStrategy/NormalMode/ASNM_dd.swift
@@ -1,66 +1,7 @@
 extension AccessibilityStrategyNormalMode {
     
     func dd(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
-        guard let element = element else { return nil }
-        
-        if let oneOfTheThreeCasesTM = handleTheThreeCasesTM(for: element) {
-            return oneOfTheThreeCasesTM
-        }
-        
-        return theMove(on: element)
-    }
-    
-    private func handleTheThreeCasesTM(for element: AccessibilityTextElement) -> AccessibilityTextElement? {
-        var element = element
-        
-        
-        if element.isEmpty {
-            element.selectedLength = 0
-            element.selectedText = nil
-            
-            return element
-        }
-        
-        if element.caretIsAtTheEnd, element.lastCharacterIsNotLinefeed {
-            element.caretLocation -= 1
-            element.selectedLength = 1
-            element.selectedText = nil
-            
-            _ = AccessibilityTextElementAdaptor.toAXFocusedElement(from: element)
-            guard let updatedElement = AccessibilityTextElementAdaptor.fromAXFocusedElement() else { return nil }
-            
-            element = updatedElement
-            
-            return theMove(on: element)
-        }
-        
-        if element.caretIsAtTheEnd, element.lastCharacterIsLinefeed {
-            element.caretLocation -= 1
-            element.selectedLength = 1
-            element.selectedText = ""
-            
-            _ = AccessibilityTextElementAdaptor.toAXFocusedElement(from: element)            
-            guard var updatedElement = AccessibilityTextElementAdaptor.fromAXFocusedElement() else { return nil }
-            
-            updatedElement.caretLocation -= 1
-            
-            _ = AccessibilityTextElementAdaptor.toAXFocusedElement(from: updatedElement)
-            guard var finalElement = AccessibilityTextElementAdaptor.fromAXFocusedElement() else { return nil }
-            
-            let firstNonBlankWithinLimitOfLastLineLocation = textEngine.firstNonBlankWithinLineLimit(in: TextEngineLine(from: finalElement.currentLine.value))            
-            finalElement.caretLocation = finalElement.currentLine.start + firstNonBlankWithinLimitOfLastLineLocation
-            finalElement.selectedLength = 0
-            finalElement.selectedText = nil
-            
-            return finalElement
-        }
-        
-        
-        return nil
-    }
-    
-    private func theMove(on element: AccessibilityTextElement) -> AccessibilityTextElement {
-        var element = element
+        guard var element = element else { return nil }
                 
         if let nextLine = AccessibilityTextElementAdaptor.lineFor(lineNumber: element.currentLine.number + 1) {
             let lineValue = nextLine.value
