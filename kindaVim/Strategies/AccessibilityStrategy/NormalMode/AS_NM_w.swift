325c3048095e3163c9007ddbda54722631e365da
Guill Lo <guill.bout@gmail.com>
2021-07-21 16:37:34 +0800

    update NM w to handle emojis, tests and impl

--- kindaVim/Strategies/AccessibilityStrategy/NormalMode/AS_NM_w.swift
+++ kindaVim/Strategies/AccessibilityStrategy/NormalMode/AS_NM_w.swift
@@ -1,13 +1,14 @@
 extension AccessibilityStrategyNormalMode {
     
     func w(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
-        guard var element = element else { return nil }
+        guard let element = element else { return nil }
+        var newElement = element
         
-        element.caretLocation = textEngine.beginningOfWordForward(startingAt: element.caretLocation, in: TextEngineText(from: element.value))
-        element.selectedLength = 1
-        element.selectedText = nil
+        newElement.caretLocation = textEngine.beginningOfWordForward(startingAt: element.caretLocation, in: TextEngineText(from: element.value))
+        newElement.selectedLength = newElement.characterLength
+        newElement.selectedText = nil
         
-        return element
+        return newElement
     }
     
 }
