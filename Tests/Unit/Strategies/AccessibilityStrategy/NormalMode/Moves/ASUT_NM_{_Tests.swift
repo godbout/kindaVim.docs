feacdb0a93b27129b70350943c882bb823352208
Guill Lo <guill.bout@gmail.com>
2021-07-21 23:29:42 +0800

    update NM { to handle emojis, impl and tests

--- Tests/Unit/Strategies/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_{_Tests.swift
+++ Tests/Unit/Strategies/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_{_Tests.swift
@@ -2,46 +2,46 @@
 import XCTest
 
 
-class ASNM_leftBrace_Tests: ASNM_BaseTests {
+// see [( for blah blah blah
+class ASUT_NM_LeftBrace_Tests: ASNM_BaseTests {
     
-    let textEngineMock = TextEngineMock()
-    
-    override func setUp() {
-        super.setUp()
-        
-        asNormalMode = AccessibilityStrategyNormalMode(textEngine: textEngineMock)
-    }    
+    private func applyMove(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
+        return asNormalMode.leftBrace(on: element) 
+    }
     
 }
 
 
-// Both
-extension ASNM_leftBrace_Tests {
+// emojis
+extension ASUT_NM_LeftBrace_Tests {
     
-    func test_that_it_calls_the_TextEngine_beginningOfParagraphBackward_function() {
+    func test_that_it_returns_the_correct_caretLocation_and_selectedLength() {
         let text = """
-just testing call to beginningOfParagraphBackward
-all the cases are tested in
-the TextEngineTests
+y{ah 🤨️(🤨️ coz🤨️🤨️ the text 🤨️🤨️functions don't
+
+care about😂️🤨️🤨️🤨️ the length but 🦋️ the move
+itself d🤨️🤨️🤨️oes
 """
         let element = AccessibilityTextElement(
             role: .textArea,
             value: text,
-            length: 97,
-            caretLocation: 60,
-            selectedLength: 1,
-            selectedText: "s",
+            length: 126,
+            caretLocation: 120,
+            selectedLength: 3,
+            selectedText: "🤨️",
             currentLine: AccessibilityTextElementLine(
                 fullValue: text,
-                number: 2,
-                start: 50,
-                end: 78
+                number: 4,
+                start: 106,
+                end: 126
             )
         )
         
-        _ = asNormalMode.leftBrace(on: element)
+        let returnedElement = applyMove(on: element)
         
-        XCTAssertEqual(textEngineMock.functionCalled, "beginningOfParagraphBackward(startingAt:in:)")        
+        XCTAssertEqual(returnedElement?.caretLocation, 54)
+        XCTAssertEqual(returnedElement?.selectedLength, 1)
     }
     
 }
+
--- kindaVim/Strategies/AccessibilityStrategy/NormalMode/AS_NM_{.swift
+++ kindaVim/Strategies/AccessibilityStrategy/NormalMode/AS_NM_{.swift
@@ -1,13 +1,14 @@
 extension AccessibilityStrategyNormalMode {
     
     func leftBrace(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
-        guard var element = element else { return nil }
+        guard let element = element else { return nil }
+        var newElement = element
  
-        element.caretLocation = textEngine.beginningOfParagraphBackward(startingAt: element.caretLocation, in: TextEngineText(from: element.value))
-        element.selectedLength = 1
-        element.selectedText = nil
+        newElement.caretLocation = textEngine.beginningOfParagraphBackward(startingAt: element.caretLocation, in: TextEngineText(from: element.value))
+        newElement.selectedLength = newElement.characterLength
+        newElement.selectedText = nil
         
-        return element
+        return newElement
     }
     
 }
