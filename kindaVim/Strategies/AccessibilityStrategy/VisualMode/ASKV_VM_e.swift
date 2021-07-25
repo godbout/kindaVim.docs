78fb84f0a1e740d6cb2771261ed1de178b6aa125
Guill Lo <guill.bout@gmail.com>
2021-07-25 18:38:04 +0800

    update VM e to use newElement

--- kindaVim/Strategies/AccessibilityStrategy/VisualMode/ASKV_VM_e.swift
+++ kindaVim/Strategies/AccessibilityStrategy/VisualMode/ASKV_VM_e.swift
@@ -1,28 +1,29 @@
 extension AccessibilityStrategyVisualMode {
     
     func e(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
-        guard var element = element else { return nil }
+        guard let element = element else { return nil }
+        var newElement = element
         
         if KindaVimEngine.shared.visualStyle == .characterwise {
             if Self.head >= Self.anchor {
                 let endOfWordForwardLocation = textEngine.endOfWordForward(startingAt: (element.caretLocation + element.selectedLength) - element.characterLengthForCharacter(at: element.caretLocation + element.selectedLength), in: TextEngineText(from: element.value))                
-                element.selectedLength += (endOfWordForwardLocation + element.characterLengthForCharacter(at: endOfWordForwardLocation)) - (Self.head + element.characterLengthForCharacter(at: Self.head))
+                newElement.selectedLength += (endOfWordForwardLocation + element.characterLengthForCharacter(at: endOfWordForwardLocation)) - (Self.head + element.characterLengthForCharacter(at: Self.head))
             } else if Self.head < Self.anchor {
                 let endOfWordForwardLocation = textEngine.endOfWordForward(startingAt: element.caretLocation, in: TextEngineText(from: element.value))
                 
                 if endOfWordForwardLocation > Self.anchor {
-                    element.caretLocation = Self.anchor
-                    element.selectedLength = (endOfWordForwardLocation + element.characterLengthForCharacter(at: endOfWordForwardLocation)) - Self.anchor              
+                    newElement.caretLocation = Self.anchor
+                    newElement.selectedLength = (endOfWordForwardLocation + element.characterLengthForCharacter(at: endOfWordForwardLocation)) - Self.anchor              
                 } else {
-                    element.caretLocation = endOfWordForwardLocation
-                    element.selectedLength = (Self.anchor + element.characterLengthForCharacter(at: Self.anchor)) - endOfWordForwardLocation
+                    newElement.caretLocation = endOfWordForwardLocation
+                    newElement.selectedLength = (Self.anchor + element.characterLengthForCharacter(at: Self.anchor)) - endOfWordForwardLocation
                 }
             }
         }
         
-        element.selectedText = nil
+        newElement.selectedText = nil
         
-        return element
+        return newElement
     }
      
 }
