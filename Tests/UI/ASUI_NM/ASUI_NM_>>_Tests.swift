37ee884b17fa24a88fda095096942e2038b40e33
Guill Lo <guill.bout@gmail.com>
2021-07-26 18:14:47 +0800

    add NM >>, impl and tests

---
+++ Tests/UI/ASUI_NM/ASUI_NM_>>_Tests.swift
@@ -0,0 +1,63 @@
+@testable import kindaVim
+import XCTest
+
+
+class ASUI_NM_rightChevronRightChevron_Tests: ASUI_NM_BaseTests {
+    
+    private func applyMoveAndGetBackAccessibilityElement() -> AccessibilityTextElement? {
+        KindaVimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .rightChevron))
+        KindaVimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .rightChevron))
+        
+        return AccessibilityTextElementAdaptor.fromAXFocusedElement()
+    }
+    
+}
+
+
+// Both
+extension ASUI_NM_rightChevronRightChevron_Tests {
+    
+    func test_that_in_normal_setting_it_adds_4_spaces_at_the_beginning_of_a_line_and_sets_the_caret_to_the_first_non_blank_of_the_line() {
+        let textInAXFocusedElement = """
+seems that even the normal
+🖕️ase fails LMAO
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        KindaVimEngine.shared.enterNormalMode()
+        
+        let accessibilityElement = applyMoveAndGetBackAccessibilityElement()
+        
+        XCTAssertEqual(accessibilityElement?.value, """
+seems that even the normal
+    🖕️ase fails LMAO
+"""
+        )        
+        XCTAssertEqual(accessibilityElement?.caretLocation, 31)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 3)
+    }
+    
+    func test_that_it_does_not_shift_the_line_if_the_line_is_considered_empty() {
+        let textInAXFocusedElement = """
+a line empty means with nothing
+
+or just a linefeed
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        KindaVimEngine.shared.enterNormalMode()
+        KindaVimEngine.shared.handle(keyCombination: KeyCombination(key: .k))
+        
+        let accessibilityElement = applyMoveAndGetBackAccessibilityElement()
+        
+        XCTAssertEqual(accessibilityElement?.value, """
+a line empty means with nothing
+
+or just a linefeed
+"""
+        )
+        XCTAssertEqual(accessibilityElement?.caretLocation, 32)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 1)
+    }
+    
+}
--- kindaVim/Strategies/AccessibilityStrategy/NormalMode/ASKV_NM_>>.swift
+++ kindaVim/Strategies/AccessibilityStrategy/NormalMode/ASKV_NM_>>.swift
@@ -4,7 +4,26 @@ extension AccessibilityStrategyNormalMode {
         guard let element = element else { return nil }
         var newElement = element
         
-        return newElement
+        guard newElement.currentLine.isNotAnEmptyLine else {
+            newElement.caretLocation = element.currentLine.start
+            newElement.selectedLength = 1
+            newElement.selectedText = nil
+            
+            return newElement
+        }
+        
+        newElement.caretLocation = element.currentLine.start
+        newElement.selectedLength = 0
+        newElement.selectedText = "    "
+        
+        _ = AccessibilityTextElementAdaptor.toAXFocusedElement(from: newElement)
+        guard var updatedElement = AccessibilityTextElementAdaptor.fromAXFocusedElement() else { return nil }
+        
+        updatedElement.caretLocation = updatedElement.currentLine.start + textEngine.firstNonBlankWithinLineLimit(in: TextEngineLine(from: updatedElement.currentLine.value))
+        updatedElement.selectedLength = updatedElement.characterLength
+        updatedElement.selectedText = nil
+        
+        return updatedElement
     }
     
 }
