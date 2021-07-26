50c3c51b7d270b6159778677a693fcc39ccdf3f8
Guill Lo <guill.bout@gmail.com>
2021-07-26 19:55:14 +0800

    add NM <<, impl and tests

---
+++ Tests/UI/ASUI_NM/ASUI_NM_<<_Tests.swift
@@ -0,0 +1,63 @@
+@testable import kindaVim
+import XCTest
+
+
+class ASUI_NM_leftChevronLeftChevron_Tests: ASUI_NM_BaseTests {
+    
+    private func applyMoveAndGetBackAccessibilityElement() -> AccessibilityTextElement? {
+        KindaVimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .leftChevron))
+        KindaVimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .leftChevron))
+        
+        return AccessibilityTextElementAdaptor.fromAXFocusedElement()
+    }
+    
+}
+
+
+// Both
+extension ASUI_NM_leftChevronLeftChevron_Tests {
+    
+    func test_that_in_normal_setting_it_removes_4_spaces_at_the_beginning_of_a_line_and_sets_the_caret_to_the_first_non_blank_of_the_line() {
+        let textInAXFocusedElement = """
+seems that even the normal
+       🖕️ase fails LMAO
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        KindaVimEngine.shared.enterNormalMode()
+        
+        let accessibilityElement = applyMoveAndGetBackAccessibilityElement()
+        
+        XCTAssertEqual(accessibilityElement?.value, """
+seems that even the normal
+   🖕️ase fails LMAO
+"""
+        )        
+        XCTAssertEqual(accessibilityElement?.caretLocation, 30)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 3)
+    }
+    
+    func test_that_it_removes_all_the_spaces_if_there_are_4_or_less() {
+        let textInAXFocusedElement = """
+ now the line
+  😀️as just two
+ spaces
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        KindaVimEngine.shared.enterNormalMode()
+        KindaVimEngine.shared.handle(keyCombination: KeyCombination(key: .k))
+        
+        let accessibilityElement = applyMoveAndGetBackAccessibilityElement()
+        
+        XCTAssertEqual(accessibilityElement?.value, """
+ now the line
+😀️as just two
+ spaces
+"""
+        )
+        XCTAssertEqual(accessibilityElement?.caretLocation, 14)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 3)
+    }
+    
+}
--- kindaVim/Strategies/AccessibilityStrategy/NormalMode/ASKV_NM_<<.swift
+++ kindaVim/Strategies/AccessibilityStrategy/NormalMode/ASKV_NM_<<.swift
@@ -4,7 +4,20 @@ extension AccessibilityStrategyNormalMode {
         guard let element = element else { return nil }
         var newElement = element
         
-        return newElement
+        let leadingNonBlanksCount = textEngine.firstNonBlankWithinLineLimit(in: TextEngineLine(from: element.currentLine.value))
+        
+        newElement.caretLocation = element.currentLine.start
+        newElement.selectedLength = leadingNonBlanksCount >= 4 ? 4 : leadingNonBlanksCount
+        newElement.selectedText = ""
+        
+        _ = AccessibilityTextElementAdaptor.toAXFocusedElement(from: newElement)
+        guard var updatedElement = AccessibilityTextElementAdaptor.fromAXFocusedElement() else { return nil }
+        
+        updatedElement.caretLocation = updatedElement.currentLine.start + textEngine.firstNonBlankWithinLineLimit(in: TextEngineLine(from: updatedElement.currentLine.value))
+        updatedElement.selectedLength = updatedElement.characterLength
+        updatedElement.selectedText = nil
+        
+        return updatedElement
     }
     
 }
