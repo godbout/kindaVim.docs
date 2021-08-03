e298dd12995d2b31da8c5870734b7a51a5b13cb7
Guill Lo <guill.bout@gmail.com>
2021-08-03 18:44:58 +0800

    make KVE enter IM when switching Spaces

--- kindaVim/AppDelegate.swift
+++ kindaVim/AppDelegate.swift
@@ -6,7 +6,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     lazy var appComponent = AppComponent()
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        appComponent.setUp()        
+        appComponent.setUp()
+        
+        NSWorkspace.shared.notificationCenter.addObserver(self, selector: #selector(self.spaceDidChange), name: NSWorkspace.activeSpaceDidChangeNotification, object: nil)
+    }
+    
+    @objc func spaceDidChange() {
+        KindaVimEngine.shared.enterInsertMode()
     }
 
 }
