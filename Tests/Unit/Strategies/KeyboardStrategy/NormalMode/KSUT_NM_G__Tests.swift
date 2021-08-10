6639bf975d27dfba9c54088faa4685b00a4b71dc
Guill Lo <guill.bout@gmail.com>
2021-08-10 16:22:38 +0800

    add back better KS NM G move for WebArea

--- Tests/Unit/Strategies/KeyboardStrategy/NormalMode/KSUT_NM_G__Tests.swift
+++ Tests/Unit/Strategies/KeyboardStrategy/NormalMode/KSUT_NM_G__Tests.swift
@@ -8,8 +8,9 @@ class KSUT_NM_G__Tests: KSUT_BaseTests {
     func test_that_for_AXWebArea_G_returns_command_down() {
         let keyCombinations = ksNormalMode.G(on: .webArea)
 
-        guard keyCombinations.count == 1 else { return XCTFail() }
+        guard keyCombinations.count == 2 else { return XCTFail() }
         XCTAssertEqual(keyCombinations[0], KeyCombination(key: .down, command: true))
+        XCTAssertEqual(keyCombinations[1], KeyCombination(key: .left, command: true))
     }
     
     func test_that_for_other_roles_G_returns_option_down() {
--- kindaVim/Strategies/KeyboardStrategy/NormalMode/KSKV_NM_G_.swift
+++ kindaVim/Strategies/KeyboardStrategy/NormalMode/KSKV_NM_G_.swift
@@ -3,7 +3,10 @@ extension KeyboardStrategyNormalMode {
     func G(on role: AXElementRole?) -> [KeyCombination] {
         switch role {
         case .webArea:
-            return [KeyCombination(key: .down, command: true)]
+            return [
+                KeyCombination(key: .down, command: true),
+                KeyCombination(key: .left, command: true),
+            ]
         default:
             return [KeyCombination(key: .down, option: true)]
         }
