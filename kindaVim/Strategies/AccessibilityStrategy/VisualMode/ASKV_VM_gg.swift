892f8c7e2c0dcd7d4d9bdc7da4b1931d186fb9b4
Guill Lo <guill.bout@gmail.com>
2021-08-12 00:06:55 +0800

    add AS VM gg C, impl and tests

--- kindaVim/Strategies/AccessibilityStrategy/VisualMode/ASKV_VM_gg.swift
+++ kindaVim/Strategies/AccessibilityStrategy/VisualMode/ASKV_VM_gg.swift
@@ -17,7 +17,27 @@ extension AccessibilityStrategyVisualMode {
     private func ggForVisualModeCharacterwise(on element: AccessibilityTextElement) -> AccessibilityTextElement {
         var newElement = element
         
-        return newElement        
+        guard let firstLine = AccessibilityTextElementAdaptor.lineFor(location: 0) else {
+            newElement.selectedLength = element.characterLength
+            newElement.selectedText = nil
+            
+            return newElement
+        }
+        
+        let firstNonBlankWithinLineLimitOfFirstLine = textEngine.firstNonBlankWithinLineLimit(in: TextEngineLine(from: firstLine.value))
+        let newHeadLocation = firstLine.start + firstNonBlankWithinLineLimitOfFirstLine
+        
+        if newHeadLocation >= Self.anchor {
+            newElement.caretLocation = Self.anchor
+            newElement.selectedLength = (newHeadLocation + element.characterLengthForCharacter(at: newHeadLocation)) - newElement.caretLocation 
+        } else {
+            newElement.caretLocation = newHeadLocation
+            newElement.selectedLength = (Self.anchor + element.characterLengthForCharacter(at: Self.anchor)) - newHeadLocation
+        }
+        
+        newElement.selectedText = nil
+        
+        return newElement  
     }
     
     private func ggForVisualModeLinewise(on element: AccessibilityTextElement) -> AccessibilityTextElement {
