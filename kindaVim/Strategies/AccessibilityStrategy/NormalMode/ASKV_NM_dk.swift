9b272c27b94b266bd0665ea569c0a8d47ffd13c1
Guill Lo <guill.bout@gmail.com>
2021-08-14 19:55:44 +0800

    add AS dk, impl and tests

--- kindaVim/Strategies/AccessibilityStrategy/NormalMode/ASKV_NM_dk.swift
+++ kindaVim/Strategies/AccessibilityStrategy/NormalMode/ASKV_NM_dk.swift
@@ -1,7 +1,32 @@
 extension AccessibilityStrategyNormalMode {
     
     func dk(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
-        return element
+        guard let element = element else { return nil }
+        var newElement = element
+        
+        if let previousLine = AccessibilityTextElementAdaptor.lineFor(lineNumber: element.currentLine.number - 1) {
+            newElement.caretLocation = previousLine.start
+            newElement.selectedLength = previousLine.length + newElement.currentLine.length
+            newElement.selectedText = ""
+            
+            _ = AccessibilityTextElementAdaptor.toAXFocusedElement(from: newElement)
+            
+            if let updatedElement = AccessibilityTextElementAdaptor.fromAXFocusedElement() {            
+                let firstNonBlankWithinLineLimitOfUpdatedElementLocation = textEngine.firstNonBlankWithinLineLimit(in: TextEngineLine(from: updatedElement.currentLine.value))
+                
+                newElement.caretLocation += firstNonBlankWithinLineLimitOfUpdatedElementLocation
+            }
+            
+            newElement.selectedLength = newElement.characterLength
+            newElement.selectedText = nil
+            
+            return newElement
+        }    
+        
+        newElement.selectedLength = element.characterLength
+        newElement.selectedText = nil
+        
+        return newElement
     }
     
 }
