80a89514045518f87942c75e7f4e6311e09794f7
Guill Lo <guill.bout@gmail.com>
2021-08-15 16:27:22 +0800

    move ciInnerBrackets ASUT tests to ASUI for linefeed right after opening brackets

--- Tests/UI/ASUI_NM/Moves/ASUI_NM_ci(_Tests.swift
+++ Tests/UI/ASUI_NM/Moves/ASUI_NM_ci(_Tests.swift
@@ -4,11 +4,11 @@ import XCTest
 
 // see ci" for blah blah 
 // plus we don't test ci[, ci{ and ci)]} because in KVE it's copy/pasted
-// and the UI Tests are expensive :(
-// also we have another test that checks that we position the caret properly in a special
-// case where we follow autoindentation and first non blank blah blah. currently only this
-// part of the move is calling lineFor, which requires a UI Test.
-class UIASNM_ciLeftParenthesis_Tests: ASUI_NM_BaseTests {
+// and the UI Tests are
+// also we have others tests here for when the opening bracket is followed by
+// a linefeed. this is because we need to call AX.lineFor to get info about the
+// next line. that's the only case where we need UI tests. the rest is in UT.
+class ASUI_NM_ciLeftParenthesis_Tests: ASUI_NM_BaseTests {
     
     private func applyMoveAndGetBackAccessibilityElement() -> AccessibilityTextElement? {
         KindaVimEngine.shared.handle(keyCombination: KeyCombination(key: .c))
@@ -22,7 +22,29 @@ class UIASNM_ciLeftParenthesis_Tests: ASUI_NM_BaseTests {
 
 
 // Both
-extension UIASNM_ciLeftParenthesis_Tests {
+extension ASUI_NM_ciLeftParenthesis_Tests {
+    
+    func test_that_in_normal_setting_it_succeeds_and_switches_to_insert_mode() {
+        let textInAXFocusedElement = """
+hoho ( another pile of shit )
+"""
+        app.textFields.firstMatch.tap()
+        app.textFields.firstMatch.typeText(textInAXFocusedElement)
+        KindaVimEngine.shared.enterNormalMode()
+        KindaVimEngine.shared.handle(keyCombination: KeyCombination(key: .b))
+        
+        KindaVimEngine.shared.handle(keyCombination: KeyCombination(key: .c))
+        KindaVimEngine.shared.handle(keyCombination: KeyCombination(key: .i))
+        KindaVimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .leftParenthesis))
+        
+        XCTAssertEqual(KindaVimEngine.shared.currentMode, .insert)
+    }
+    
+}
+
+
+// TextViews
+extension ASUI_NM_ciLeftParenthesis_Tests {
     
     func test_that_in_normal_setting_it_can_delete_the_content_between_brackets() {
         let textInAXFocusedElement = """
@@ -46,23 +68,7 @@ hehe (
 """
         )
     }
-    
-    func test_that_in_normal_setting_it_succeeds_and_switches_to_insert_mode() {
-        let textInAXFocusedElement = """
-hoho ( another pile of shit )
-"""
-        app.textFields.firstMatch.tap()
-        app.textFields.firstMatch.typeText(textInAXFocusedElement)
-        KindaVimEngine.shared.enterNormalMode()
-        KindaVimEngine.shared.handle(keyCombination: KeyCombination(key: .b))
-        
-        KindaVimEngine.shared.handle(keyCombination: KeyCombination(key: .c))
-        KindaVimEngine.shared.handle(keyCombination: KeyCombination(key: .i))
-        KindaVimEngine.shared.handle(keyCombination: KeyCombination(vimKey: .leftParenthesis))
-        
-        XCTAssertEqual(KindaVimEngine.shared.currentMode, .insert)
-    }
-    
+
     func test_a_case_where_it_should_not_delete_the_content_and_then_stay_in_normal_mode() {
         let textInAXFocusedElement = """
 hoho no bracket on that shit
@@ -100,6 +106,49 @@ now that shit will get cleaned (
 """
         )
         XCTAssertEqual(accessibilityElement?.caretLocation, 37)
+    }
+    
+    func test_that_if_the_opening_bracket_is_immediately_followed_by_a_linefeed_the_linefeed_is_not_deleted() {
+        let textInAXFocusedElement = """
+this work when (
+is followed by a linefeed
+and ) is not preceded by a linefeed
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        KindaVimEngine.shared.enterNormalMode()
+        KindaVimEngine.shared.handle(keyCombination: KeyCombination(key: .k))
+        
+        let accessibilityElement = applyMoveAndGetBackAccessibilityElement()
+        
+        XCTAssertEqual(accessibilityElement?.value, """
+this work when (
+) is not preceded by a linefeed
+"""
+        )
+        XCTAssertEqual(accessibilityElement?.caretLocation, 17)
+    }
+
+    func test_that_if_the_opening_bracket_is_immediately_followed_by_a_linefeed_and_the_closing_bracket_is_immediately_preceded_by_a_linefeed_then_the_move_keeps_an_empty_line_between_the_brackets() {
+        let textInAXFocusedElement = """
+this case is when (
+is followed by a linefeed and
+) is preceded by a linefeed
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        KindaVimEngine.shared.enterNormalMode()
+        KindaVimEngine.shared.handle(keyCombination: KeyCombination(key: .k))
+        
+        let accessibilityElement = applyMoveAndGetBackAccessibilityElement()
+        
+        XCTAssertEqual(accessibilityElement?.value, """
+this case is when (
+
+) is preceded by a linefeed
+"""
+        )
+        XCTAssertEqual(accessibilityElement?.caretLocation, 20)
     }
     
 }
--- Tests/Unit/Strategies/AccessibilityStrategy/NormalMode/Helpers/ASUT_NM_ciInnerBrackets_Tests.swift
+++ Tests/Unit/Strategies/AccessibilityStrategy/NormalMode/Helpers/ASUT_NM_ciInnerBrackets_Tests.swift
@@ -7,7 +7,8 @@ import XCTest
 // spans on a line or several. this is up to ciInnerBrackets to handle this, which is
 // what we test here.
 // also so other stuff like going back to IM or NM have to be tested through UI.
-// also one of the test where we reposition the cursor to the first non blank need to be done through UI.
+// also some tests need to be moved to UI, i.e. when the { is followed by a linefeed, because
+// we need to query the AX to get info about the next line.
 class ASUT_NM_ciInnerBrackets_Tests: ASNM_BaseTests {
     
     private func applyMove(using bracket: Character, on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
@@ -77,35 +78,7 @@ and } is not preceded by a linefeed
         XCTAssertEqual(returnedElement?.selectedLength, 35)
         XCTAssertEqual(returnedElement?.selectedText, "")
     }
-    
-    func test_that_if_the_opening_bracket_is_immediately_followed_by_a_linefeed_the_linefeed_is_not_deleted() {
-        let text = """
-this work when {
-is followed by a linefeed
-and } is not preceded by a linefeed
-"""
-        let element = AccessibilityTextElement(
-            role: .textArea,
-            value: text,
-            length: 78,
-            caretLocation: 29,
-            selectedLength: 1,
-            selectedText: "b",
-            currentLine: AccessibilityTextElementLine(
-                fullValue: text,
-                number: 2,
-                start: 17,
-                end: 43
-            )
-        )
-        
-        let returnedElement = applyMove(using: "{", on: element)
-        
-        XCTAssertEqual(returnedElement?.caretLocation, 17)
-        XCTAssertEqual(returnedElement?.selectedLength, 30)
-        XCTAssertEqual(returnedElement?.selectedText, "")
-    }
-    
+
     func test_that_if_the_closing_bracket_is_preceded_only_by_whitespaces_up_to_the_beginning_of_the_line_then_the_previous_line_linefeed_is_not_deleted() {
         let text = """
 this case is when { is not followed
@@ -131,34 +104,6 @@ by a linefeed and
         
         XCTAssertEqual(returnedElement?.caretLocation, 19)
         XCTAssertEqual(returnedElement?.selectedLength, 34)
-        XCTAssertEqual(returnedElement?.selectedText, "")
-    }
-    
-    func test_that_if_the_opening_bracket_is_immediately_followed_by_a_linefeed_and_the_closing_bracket_is_immediately_preceded_by_a_linefeed_then_the_move_keeps_an_empty_line_between_the_brackets() {
-        let text = """
-this case is when {
-is followed by a linefeed and
-} is preceded by a linefeed
-"""
-        let element = AccessibilityTextElement(
-            role: .textArea,
-            value: text,
-            length: 77,
-            caretLocation: 36,
-            selectedLength: 1,
-            selectedText: " ",
-            currentLine: AccessibilityTextElementLine(
-                fullValue: text,
-                number: 2,
-                start: 20,
-                end: 50
-            )
-        )
-        
-        let returnedElement = applyMove(using: "{", on: element)
-        
-        XCTAssertEqual(returnedElement?.caretLocation, 20)
-        XCTAssertEqual(returnedElement?.selectedLength, 29)
         XCTAssertEqual(returnedElement?.selectedText, "")
     }
 
