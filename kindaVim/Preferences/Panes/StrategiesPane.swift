ab590ab7d9acd977b501d60766f457b7c41716a1
Guill Lo <guill.bout@gmail.com>
2021-08-24 23:14:42 +0800

    add some text in Preferences Strategies to say we're hardcoding for now

--- kindaVim/Preferences/Panes/StrategiesPane.swift
+++ kindaVim/Preferences/Panes/StrategiesPane.swift
@@ -5,7 +5,9 @@ import Preferences
 struct StrategiesPane: View {
     
     var body: some View {
-        Preferences.Container(contentWidth: 450.0) {}
+        Preferences.Container(contentWidth: 450.0) {
+            Preferences.Section(title: "currently harcoded LMAO") {}
+        }
     }
     
 }
