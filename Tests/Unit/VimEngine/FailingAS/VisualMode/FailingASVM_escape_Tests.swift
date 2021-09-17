fb1aff39cb6a1c62c6497d86c31ec989a84adc24
Guill Lo <guill.bout@gmail.com>
2021-09-17 13:52:07 +0800

    fix filename of Failing ASVM escape

---
+++ Tests/Unit/VimEngine/FailingAS/VisualMode/FailingASVM_escape_Tests.swift
@@ -0,0 +1,30 @@
+@testable import kindaVim
+import KeyCombination
+import XCTest
+
+
+class FailingASVM_escape_Tests: FailingASVM_BaseTests {
+    
+    private func applyKeyCombinationBeingTested() {
+        KindaVimEngine.shared.handle(keyCombination: KeyCombination(key: .escape))
+    }
+        
+}
+
+
+// Both
+extension FailingASVM_escape_Tests {
+    
+    func test_that_it_currently_calls_nothing() {
+        applyKeyCombinationBeingTested()
+        
+        XCTAssertEqual(ksVisualModeMock.functionCalled, "")
+    }
+    
+    func test_that_if_Vim_was_in_visual_mode_linewise_it_switches_into_insert_mode() {
+        applyKeyCombinationBeingTested()
+        
+        XCTAssertEqual(KindaVimEngine.shared.currentMode, .insert)
+    }
+        
+}
