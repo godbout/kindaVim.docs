447793a763daaaa75dedbe9eb58caece05a3ff4c
Guill Lo <guill.bout@gmail.com>
2021-09-25 14:40:27 +0800

    add a SettingsView to be called from the Settings Scene

---
+++ kindaVim/Preferences/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  kindaVim
+//
+//  Created by Guillaume Leclerc on 25/09/2021.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
--- kindaVim/kindaVimApp.swift
+++ kindaVim/kindaVimApp.swift
@@ -8,7 +8,7 @@ struct kindaVimApp: App {
 
     var body: some Scene {
         Settings {
-            EmptyView()
+            SettingsView()
         }
     }
     
