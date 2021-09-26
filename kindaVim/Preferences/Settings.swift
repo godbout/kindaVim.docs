7137b79e62c409226ea4df468030f5a59159267c
Guill Lo <guill.bout@gmail.com>
2021-09-26 14:13:56 +0800

    "refactor" Settings to be more understandable by me first
    will probably need to refactor truly later tho.

--- kindaVim/Preferences/Settings.swift
+++ kindaVim/Preferences/Settings.swift
@@ -18,13 +18,15 @@ struct SettingsKeys {
 extension Array: RawRepresentable where Element: Codable {
 
     public init?(rawValue: String) {
-        guard let data = rawValue.data(using: .utf8), let result = try? JSONDecoder().decode([Element].self, from: data) else { return nil}
+        guard let data = rawValue.data(using: .utf8) else { return nil }
+        guard let result = try? JSONDecoder().decode([Element].self, from: data) else { return nil}
         
         self = result
     }
 
     public var rawValue: String {
-        guard let data = try? JSONEncoder().encode(self), let result = String(data: data, encoding: .utf8) else { return "[]" }
+        guard let data = try? JSONEncoder().encode(self) else { return "[]" }
+        guard let result = String(data: data, encoding: .utf8) else { return "[]" }
         
         return result
     }
