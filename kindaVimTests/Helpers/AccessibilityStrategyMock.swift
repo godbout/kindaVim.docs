67f5a7ae0a938c03c39a087bcd31b3cdd071c602
Guill Lo <guill.bout@gmail.com>
2021-10-01 18:57:34 +0800

    update AS dep

--- kindaVimTests/Helpers/AccessibilityStrategyMock.swift
+++ kindaVimTests/Helpers/AccessibilityStrategyMock.swift
@@ -6,7 +6,21 @@ import AccessibilityStrategy
 struct AccessibilityStrategyMock: AccessibilityStrategyProtocol {
 
     func focusedTextElement() -> AccessibilityTextElement? {
-        return AccessibilityTextElement(value: "", length: 0, caretLocation: 0, selectedLength: 0)
+        return AccessibilityTextElement(
+            role: .someOtherShit,
+            value: "",
+            length: 0,
+            caretLocation: 0,
+            selectedLength: 0,
+            selectedText: "",
+            currentLine: AccessibilityTextElementLine(
+                fullTextValue: "",
+                fullTextLength: 0,
+                number: 1,
+                start: 0,
+                end: 0
+            )
+        )
     }
     
     func push(element: AccessibilityTextElement) -> Bool {
