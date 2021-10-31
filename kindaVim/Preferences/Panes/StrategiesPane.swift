0705399995c996bf0101fd4d1b93ed5fa26fe2f1
Guill Lo <guill.bout@gmail.com>
2021-10-31 11:36:19 +0800

    make UI for Strategies Pane for v1

--- kindaVim/Preferences/Panes/StrategiesPane.swift
+++ kindaVim/Preferences/Panes/StrategiesPane.swift
@@ -4,17 +4,62 @@ import SwiftUI
 struct StrategiesPane: View {
     
     @AppStorage(SettingsKeys.jkMapping) private var jkMapping: Bool = true
+    @AppStorage(SettingsKeys.appsToIgnore) private var appsToIgnore: [String] = [
+        "com.sublimetext.4",
+        "com.googlecode.iterm2",
+        "com.microsoft.VSCode",
+        "com.jetbrains.PhpStorm",
+        "com.github.atom"
+    ]
+    @AppStorage(SettingsKeys.appsForWhichToEnforceKeyboardStrategy) private var appsForWhichToEnforceKeyboardStrategy: [String] = [
+        "com.apple.Safari"
+    ]
+
+    @State private var selection = Set<String>()
     
     var body: some View {
         
         Form {
-            VStack(alignment: .leading) {
-                Text("currently hardcoded LMAO")
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("drop below the apps that you want kV to ignore:")
+                        .padding(.bottom, 1)
+                    Text("useful for apps that already have a Vim mode like Sublime Text, iTerm2, etc.")
+                        .padding(.leading, 5)
+                        .font(.footnote)
+                        .foregroundColor(.gray)
+                    List(appsToIgnore, id: \.self, selection: $selection) {
+                        Text($0)
+                    }
+                    .listStyle(.bordered(alternatesRowBackgrounds: true))
+                }
+
+                Spacer()
+
+                VStack(alignment: .leading) {
+                    Text("drop below the apps for which you want kV to enforce the Keyboard Strategy:")
+                        .padding(.bottom, 1)
+                    Text("currently useful for fake shit pretending apps like all Electron apps, and restricted apps like browsers.")
+                        .padding(.leading, 5)
+                        .font(.footnote)
+                        .foregroundColor(.gray)
+                    List(appsForWhichToEnforceKeyboardStrategy, id: \.self) {
+                        Text($0)
+                    }
+                    .listStyle(.bordered)
+                }
+            }
+
+            Divider()
                 
-                Toggle("map jk gj gk", isOn: $jkMapping)
+            HStack {
+                Spacer()
+
+                Toggle("map jk to gj gk for Accessibility Strategy", isOn: $jkMapping)
                     .onChange(of: jkMapping) { 
                         KindaVimEngine.shared.jkMapping = $0
                     }
+
                 Spacer()
             }
             .padding(.top, 15)
