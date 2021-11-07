7169fd77627cef9bd38f990488dbe87ff18ce840
Guill Lo <guill.bout@gmail.com>
2021-11-07 23:52:00 +0800

    --wip-- [skip ci]

--- kindaVim/Preferences/Panes/VisualsPane.swift
+++ kindaVim/Preferences/Panes/VisualsPane.swift
@@ -4,16 +4,18 @@ import SwiftUI
 struct VisualsPane: View {
 
     @AppStorage(SettingsKeys.toggleHazeOverWindow) private var toggleHazeOverWindow: Bool = true
+    @AppStorage(SettingsKeys.toggleMenuBarIcon) private var toggleMenuBarIcon: Bool = false
     @AppStorage(SettingsKeys.showCharactersTyped) private var showCharactersTyped: Bool = false
 
     var body: some View {
         
         Form {
             VStack(alignment: .leading) {
-                Toggle("Focus the window that's Viming", isOn: $toggleHazeOverWindow)
+                Toggle("Highlight the window that is Viming", isOn: $toggleHazeOverWindow)
                     .onChange(of: toggleHazeOverWindow) { 
                         KindaVimEngine.shared.toggleHazeOverWindow = $0
                     }
+                Toggle("Change Menu Bar Icon when not Viming", isOn: $toggleMenuBarIcon)
 //                Toggle("Show characters typed", isOn: $showCharactersTyped)
 //                    .onChange(of: showCharactersTyped) {
 //                        KindaVimEngine.shared.showCharactersTyped = $0
