57c5d344c39366389330b1c3e6abe3536f644495
Guill Lo <guill.bout@gmail.com>
2021-11-09 02:18:10 +0800

    update splashscreen UI

--- kindaVim/SplashScreenView.swift
+++ kindaVim/SplashScreenView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct SplashScreenView: View {
+        
     var body: some View {
         Form {
-            Text("blah blah blah yaddi yaddi yadda you know the drill. once you're granting Accessibility Privileges we will send you to the Preferences.")
-            Button("Grant Accessibility Privileges") {
-                showAccessibilityPrivilegesPrompt()
+            VStack {
+                HStack(alignment: .top) {
+                    Image(nsImage: NSImage(named: "AppIcon")!)
+                    VStack(alignment: .leading) {
+                        Text("blah blah blah you know the drill.")
+                            .font(.headline)
+                            .padding(.top)
+                        Text("once you've granted Accessibility Privileges you can enter Normal Mode with Escape, or set your own Keyboard Shorcut in the Preferences.")
+                            .font(.body)
+                            .fixedSize(horizontal: false, vertical: true)
+                            .padding(.top, 10)
+                    }
+                }
+                .padding(.bottom, 6)
+                Spacer()
+                Divider()
+                    .padding(.bottom, 6)
+                HStack {
+                    Button("Quit kindaVim") {
+                        NSApp.terminate(self)
+                    }
+                    Spacer()
+                    Button("Grant Accessibility Privileges") {
+                        showAccessibilityPrivilegesPrompt()
+                    }
+                }        
             }
         }
-        .frame(width: 480, height: 640, alignment: .center)
+        .frame(width: 480, height: 170, alignment: .center)
+        .padding()
     }
     
     func showAccessibilityPrivilegesPrompt() {
