cfa368faa383a7980992e53f1b9f6700b3b10162
Guill Lo <guill.bout@gmail.com>
2021-11-19 19:14:37 +0800

    add handling of digits in KVE

--- kindaVim/VimEngine/KindaVimEngine.swift
+++ kindaVim/VimEngine/KindaVimEngine.swift
@@ -86,26 +86,95 @@ class KindaVimEngine {
             display.showKeysTyped(lastBeing: keyCombination)
         }
         
-        switch (currentMode, enforceKeyboardStrategy) {
-        case (.normal, false):
-            handleNormalMode(using: .accessibilityStrategy, for: keyCombination)
-        case (.normal, true):
-            handleNormalMode(using: .keyboardStrategy, for: keyCombination)
-        case (.operatorPendingForNormalMode, false):
-            handleOperatorPendingForNormalMode(using: .accessibilityStrategy, for: keyCombination)
-        case (.operatorPendingForNormalMode, true):
-            handleOperatorPendingForNormalMode(using: .keyboardStrategy, for: keyCombination)
-        case (.visual, false):
-            handleVisualMode(using: .accessibilityStrategy, for: keyCombination)
-        case (.visual, true):
-            handleVisualMode(using: .keyboardStrategy, for: keyCombination)
-        case (.operatorPendingForVisualMode, false):
-            handleOperatorPendingForVisualMode(using: .accessibilityStrategy, for: keyCombination)
-        case (.operatorPendingForVisualMode, true):
-            handleOperatorPendingForVisualMode(using: .keyboardStrategy, for: keyCombination)
-        default:
-            ()
+        // before sending the key combination down the rabbit hole we need to check first
+        // if it's a digit and if this digit will be used for counts. in that case the digit
+        // is not used for a move so we don't send it. else it is (like for example `r5`) and we do.
+        if grabDigitForCounts(keyCombination: keyCombination) == false {
+            switch (currentMode, enforceKeyboardStrategy) {
+            case (.normal, false):
+                handleNormalMode(using: .accessibilityStrategy, for: keyCombination)
+            case (.normal, true):
+                handleNormalMode(using: .keyboardStrategy, for: keyCombination)
+            case (.operatorPendingForNormalMode, false):
+                handleOperatorPendingForNormalMode(using: .accessibilityStrategy, for: keyCombination)
+            case (.operatorPendingForNormalMode, true):
+                handleOperatorPendingForNormalMode(using: .keyboardStrategy, for: keyCombination)
+            case (.visual, false):
+                handleVisualMode(using: .accessibilityStrategy, for: keyCombination)
+            case (.visual, true):
+                handleVisualMode(using: .keyboardStrategy, for: keyCombination)
+            case (.operatorPendingForVisualMode, false):
+                handleOperatorPendingForVisualMode(using: .accessibilityStrategy, for: keyCombination)
+            case (.operatorPendingForVisualMode, true):
+                handleOperatorPendingForVisualMode(using: .keyboardStrategy, for: keyCombination)
+            default:
+                ()
+            }
         }
+    }
+        
+    private func grabDigitForCounts(keyCombination: KeyCombination) -> Bool {
+        switch keyCombination.vimKey {
+        // 0 is special. if used after another digit, it's part of the count.
+        // if not, then it's a Vim move. we send back false when it's a Vim move
+        // so that it's treated as such.
+        case .zero:
+            if theDigitIsBeforeAVimMove() {
+                guard !firstCountBuffer.isEmpty else { return false }
+                
+                firstCountBuffer.append("0")
+                
+                return true
+            }
+            
+            if theDigitIsBetweenAnOperatorAndItsRelativeMotion() {
+                guard !secondCountBuffer.isEmpty else { return false }
+                
+                secondCountBuffer.append("0")
+                
+                return true
+            }
+                        
+            return false
+        case .one, .two, .three, .four, .five, .six, .seven, .eight, .nine:
+            if theDigitIsBeforeAVimMove() {
+                firstCountBuffer.append(keyCombination.vimKey!.rawValue)
+                            
+                return true
+            }
+            
+            if theDigitIsBetweenAnOperatorAndItsRelativeMotion() {
+                secondCountBuffer.append(keyCombination.vimKey!.rawValue)
+                            
+                return true
+            }
+                        
+            return false
+        default:
+            return false
+        }
+    }
+    
+    private func theDigitIsBeforeAVimMove() -> Bool {
+        return operatorPendingBuffer.isEmpty
+    }
+        
+    private func theDigitIsBetweenAnOperatorAndItsRelativeMotion() -> Bool {
+        // dirty for now. there are actually `operators` that are more
+        // than one key, like `g~`, `gu`, `gU` etc. if we handle those one day
+        // then the code below (and probably more) needs to change.
+        // also we may move the Vim Operators in some sort of Constant struct or enum.
+        let vimOperators = [
+            VimKey.c,
+            VimKey.d,
+            VimKey.y,
+            VimKey.leftChevron,
+            VimKey.rightChevron,
+        ]
+        
+        guard let vimKey = operatorPendingBuffer.last?.vimKey else { return false }
+        
+        return vimOperators.contains(vimKey)
     }
     
     private func handleNormalMode(using strategy: VimEngineStrategy, for keyCombination: KeyCombination) {
