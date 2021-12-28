1cb69a35e9a2a59d6388003162f143c713e366f1
Guill Lo <guill.bout@gmail.com>
2021-12-28 18:00:01 +0800

    add back input observer

--- kindaVim/Observers/InputFieldObserver.swift
+++ kindaVim/Observers/InputFieldObserver.swift
@@ -8,29 +8,28 @@ class InputFieldObserver {
     
     
     func startObserving() {
-// TODO: put back after 1b15 release
-//        guard let pid = NSWorkspace.shared.frontmostApplication?.processIdentifier else { return }
-//        
-//        if AXObserverCreate(pid, { _, _, _, _ in
-//            print("input field changed detected")
-//            AppCore.shared.vimEngine.enterInsertMode()
-//        }, &axObserver) == .success {
-//            axApplicationElement = AXUIElementCreateApplication(pid)
-//            
-//            if AXObserverAddNotification(axObserver!, axApplicationElement!, kAXFocusedUIElementChangedNotification as CFString, nil) == .success {
-//                print("notification added to run loop")
-//                CFRunLoopAddSource(CFRunLoopGetCurrent(), AXObserverGetRunLoopSource(axObserver!), .commonModes)
-//            }
-//        }
+        guard let pid = NSWorkspace.shared.frontmostApplication?.processIdentifier else { return }
+        
+        if AXObserverCreate(pid, { _, _, _, _ in
+            print("input field changed detected")
+            AppCore.shared.vimEngine.enterInsertMode()
+        }, &axObserver) == .success {
+            axApplicationElement = AXUIElementCreateApplication(pid)
+            
+            if AXObserverAddNotification(axObserver!, axApplicationElement!, kAXFocusedUIElementChangedNotification as CFString, nil) == .success {
+                print("notification added to run loop")
+                CFRunLoopAddSource(CFRunLoopGetCurrent(), AXObserverGetRunLoopSource(axObserver!), .commonModes)
+            }
+        }
     }
     
     func stopObserving() {
-//        guard let observer = axObserver, let applicationElement = axApplicationElement else { return }
-//              
-//        if AXObserverRemoveNotification(observer, applicationElement, kAXFocusedUIElementChangedNotification as CFString) == .success {
-//            print("notification removed from run loop")
-//            CFRunLoopRemoveSource(CFRunLoopGetCurrent(), AXObserverGetRunLoopSource(observer), .commonModes)
-//        }
+        guard let observer = axObserver, let applicationElement = axApplicationElement else { return }
+              
+        if AXObserverRemoveNotification(observer, applicationElement, kAXFocusedUIElementChangedNotification as CFString) == .success {
+            print("notification removed from run loop")
+            CFRunLoopRemoveSource(CFRunLoopGetCurrent(), AXObserverGetRunLoopSource(observer), .commonModes)
+        }
     }
     
 }
