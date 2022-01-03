e4b834096e32d65021b2e7ffd0923073a6f84880
Guill Lo <guill.bout@gmail.com>
2022-01-03 14:24:28 +0800

    fix UT for NM ~

--- kindaVimTests/VimEngine/Motions/SucceedingAS/NormalMode/SucceedingASNM_~_Tests.swift
+++ kindaVimTests/VimEngine/Motions/SucceedingAS/NormalMode/SucceedingASNM_~_Tests.swift
@@ -18,7 +18,7 @@ class SucceedingASNM_tilde_Tests: SucceedingASNM_BaseTests {
 extension SucceedingASNM_tilde_Tests {
 
     func test_that_it_calls_the_correct_function_on_accessibility_strategy() {
-        XCTAssertEqual(asNormalModeMock.functionCalled, "tilde(on:)")
+        XCTAssertEqual(asNormalModeMock.functionCalled, "tilde(times:on:pgR:)")
     }
 
     func test_that_it_keeps_Vim_in_normal_mode() {
