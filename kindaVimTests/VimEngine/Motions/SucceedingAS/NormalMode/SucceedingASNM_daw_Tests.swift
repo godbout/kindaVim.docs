3b07752ed06b96efb57b6ccd320789878c6f8ec7
Guill Lo <guill.bout@gmail.com>
2022-01-04 15:35:10 +0800

    update Succeeding UT for caw daw, remove pgR

--- kindaVimTests/VimEngine/Motions/SucceedingAS/NormalMode/SucceedingASNM_daw_Tests.swift
+++ kindaVimTests/VimEngine/Motions/SucceedingAS/NormalMode/SucceedingASNM_daw_Tests.swift
@@ -5,11 +5,13 @@ import XCTest
 
 class SucceedingASNM_daw_Tests: SucceedingASNM_BaseTests {
     
-    private func applyKeyCombinationsBeingTested(pgR: Bool = false) {
+    override func setUp() {
+        super.setUp()
+        
         kindaVimEngine.handle(keyCombination: KeyCombination(vimKey: .eight))
         kindaVimEngine.handle(keyCombination: KeyCombination(key: .d))
         kindaVimEngine.handle(keyCombination: KeyCombination(key: .a))
-        kindaVimEngine.handle(keyCombination: KeyCombination(key: .w), appMode: pgR == true ? .pgR : .auto)
+        kindaVimEngine.handle(keyCombination: KeyCombination(key: .w))
     }
     
 }
@@ -17,29 +19,15 @@ class SucceedingASNM_daw_Tests: SucceedingASNM_BaseTests {
 
 extension SucceedingASNM_daw_Tests {
     
-    func test_that_in_Auto_Mode_it_calls_the_correct_function_on_AS_with_PGR_off() {
-        applyKeyCombinationsBeingTested()
-                
+    func test_that_it_calls_the_correct_function_on_the_AccesssibilityStrategy() {
         XCTAssertEqual(asNormalModeMock.functionCalled, "daw(on:pgR:_:)")
-        XCTAssertEqual(asNormalModeMock.pgRPassed, false)
     }
     
-    func test_that_in_PGR_Mode_it_calls_the_correct_function_on_AS_with_PGR_on() {
-        applyKeyCombinationsBeingTested(pgR: true)
-                
-        XCTAssertEqual(asNormalModeMock.functionCalled, "daw(on:pgR:_:)")
-        XCTAssertEqual(asNormalModeMock.pgRPassed, true)
-    }
-    
-    func test_that_it_keeps_Vim_in_normal_mode() {
-        applyKeyCombinationsBeingTested()
-                
+    func test_that_it_keeps_Vim_in_NormalMode() {
         XCTAssertEqual(kindaVimEngine.currentMode, .normal)
     }
     
     func test_that_it_resets_the_count() {
-        applyKeyCombinationsBeingTested()
-                
         XCTAssertNil(kindaVimEngine.count)
     }
   
