4bffde14dda14698c73bc83b208f2f158196eacf
Guill Lo <guill.bout@gmail.com>
2022-01-25 17:57:22 +0800

    update fake Paddle License Code and remove all the Paddle tests buttons

--- kindaVim/Preferences/Panes/LicensePane.swift
+++ kindaVim/Preferences/Panes/LicensePane.swift
@@ -36,7 +36,7 @@ struct LicensePane: View {
                     }
                     HStack {
                         Spacer()
-                        TextField(text: $magicNumbers, prompt: Text("PD-f8cabe2a-9bfd-4556-a3ac-d991666c2c8f")) {
+                        TextField(text: $magicNumbers, prompt: Text("PD-a1bcde2f-3ghi-4567-j8kl-m901234n6o9f")) {
                             Label("the Magic Numbers™:", systemImage: "barcode")
                         }
                         .multilineTextAlignment(.center)
@@ -50,79 +50,7 @@ struct LicensePane: View {
                 .padding(.top, 12)
                 .foregroundColor(.gray)
                 .disabled(true)
-                    
-                Divider()
-                           
-                VStack {
-                    Button("refresh") {
-                        AppCore.shared.licensing.paddleProduct?.refresh()
-                    }
-                    Button("activate license") {
-                        AppCore.shared.licensing.paddleProduct?.activateEmail("guill.bout@gmail.com",
-                                                     license: "PD-f8cabe2a-9bfd-4556-a3ac-d991666c2c8f",
-                                                     completion: {
-                            (activated: Bool, error: Error?) in
-                            let alert: NSAlert = NSAlert()
-                            alert.messageText = String(activated)
-                            alert.addButton(withTitle: "OK")
-                            _ = alert.runModal()
-                        })
-                    }
-                    
-                    Button("check license status") {
-                        let activated = AppCore.shared.licensing.paddleProduct?.activated ?? false
-                        
-                        let alert: NSAlert = NSAlert()
-                        alert.messageText = String(activated)
-                        alert.addButton(withTitle: "OK")
-                        _ = alert.runModal()
-                    }
-                    
-                    Button("recover license") {
-                        AppCore.shared.licensing.paddle?.recoverLicense(for: AppCore.shared.licensing.paddleProduct!, email: "guill.bout@gmail.com", completion: nil)
-                    }
-                    
-                    Button("show license recovery") {
-                        AppCore.shared.licensing.paddle?.showLicenseRecovery(for: AppCore.shared.licensing.paddleProduct!, completion: nil)
-                    }
-                    
-                    Button("deactivate") {
-                        AppCore.shared.licensing.paddleProduct?.deactivate(completion: {(success, error) in
-                            let alert: NSAlert = NSAlert()
-                            alert.messageText = String(success)
-                            alert.addButton(withTitle: "OK")
-                            _ = alert.runModal()
-                        })
-                    }
-                    
-                    Button("subscription info") {
-                        let date = AppCore.shared.licensing.paddleProduct?.activationDate
-                        let email = AppCore.shared.licensing.paddleProduct?.activationEmail
-                        let activationID = AppCore.shared.licensing.paddleProduct?.activationID
-                        let lastRefreshDate = AppCore.shared.licensing.paddleProduct?.lastRefreshDate
-                        let lastVerification = AppCore.shared.licensing.paddleProduct?.lastVerifyDate
-                        let lastSuccessfulVerification = AppCore.shared.licensing.paddleProduct?.lastSuccessfulVerifiedDate
-                        let licenseCode = AppCore.shared.licensing.paddleProduct?.licenseCode
-                        let licenseExpiryDate = AppCore.shared.licensing.paddleProduct?.licenseExpiryDate
-                        
-                        let alert: NSAlert = NSAlert()
-                        alert.messageText = """
-        date: \(String(describing: date))
-        email: \(String(describing: email))
-        activationID: \(String(describing: activationID))
-        lastRefreshDate" \(String(describing: lastRefreshDate))
-        lastVerification: \(String(describing: lastVerification))
-        lastSuccessfulVerification: \(String(describing: lastSuccessfulVerification))
-        licenseCode: \(String(describing: licenseCode))
-        licenseExpiryDate: \(String(describing: licenseExpiryDate))
-        """
-                        alert.addButton(withTitle: "OK")
-                        _ = alert.runModal()
-                    }
-                }
             }
-            
-            
         }
         .frame(width: 570, height: nil)
         .padding(10)
---
+++ kindaVim/Preferences/Panes/PaddleTestingPane.swift
@@ -0,0 +1,8 @@
+//
+//  PaddleTestingPane.swift
+//  kindaVim
+//
+//  Created by Guillaume Leclerc on 25/01/2022.
+//
+
+import Foundation
