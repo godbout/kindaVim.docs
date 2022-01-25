c19b7ea57880a0526a324d2cbc15472a36da8caa
Guill Lo <guill.bout@gmail.com>
2022-01-25 17:50:04 +0800

    add an empty WindowGroup, seems necessary for enable copy/paste in Settings

--- kindaVim/kindaVimApp.swift
+++ kindaVim/kindaVimApp.swift
@@ -7,6 +7,14 @@ struct kindaVimApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
+        // dumb but seems necessary to enable
+        // copy paste in Preferences...
+        WindowGroup {
+            VStack {
+                EmptyView()
+            }
+            .hidden()
+        }
         Settings {
             SettingsView()
         }
