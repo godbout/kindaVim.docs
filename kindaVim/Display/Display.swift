8c99e568e6c2ff4562d108a5128b4e8a4019cc60
Guill Lo <guill.bout@gmail.com>
2022-02-10 19:08:10 +0800

    add new symbols for Characters Window and show Shift only for some keys

--- kindaVim/Display/Display.swift
+++ kindaVim/Display/Display.swift
@@ -34,6 +34,10 @@ struct Display {
             Self.ongoingMove.append("⌥")
         }
         
+        if keyCombination.shift == true, needToShowShiftSymbol(for: keyCombination) {
+            Self.ongoingMove.append("⇧")
+        }
+        
         if keyCombination.command == true {
             Self.ongoingMove.append("⌘")
         }
@@ -43,12 +47,20 @@ struct Display {
             Self.ongoingMove.append("↓")
         case .delete:
             Self.ongoingMove.append("⌫")
+        case .end:
+            Self.ongoingMove.append("↘")
         case .escape:
             Self.ongoingMove.append("⎋")
         case .forwardDelete:
             Self.ongoingMove.append("⌦")
+        case .home:
+            Self.ongoingMove.append("↖")
         case .leftArrow:
             Self.ongoingMove.append("←")
+        case .pageDown:
+            Self.ongoingMove.append("⇟")
+        case .pageUp:
+            Self.ongoingMove.append("⇞")
         case .return:
             Self.ongoingMove.append("↵")
         case .rightArrow:
@@ -68,6 +80,25 @@ struct Display {
         }
     }
         
+    private func needToShowShiftSymbol(for keyCombination: KeyCombination) -> Bool {
+        [
+            KeyCombination(key: .delete).character,
+            KeyCombination(key: .downArrow).character,
+            KeyCombination(key: .end).character,
+            KeyCombination(key: .escape).character,
+            KeyCombination(key: .forwardDelete).character,
+            KeyCombination(key: .leftArrow).character,
+            KeyCombination(key: .home).character,
+            KeyCombination(key: .pageDown).character,
+            KeyCombination(key: .pageUp).character,
+            KeyCombination(key: .return).character,
+            KeyCombination(key: .rightArrow).character,
+            KeyCombination(key: .space).character,
+            KeyCombination(key: .upArrow).character,
+        ]
+            .contains(keyCombination.character)
+    }
+    
     func showOngoingMove() {
         charactersWindow.show(Self.ongoingMove)
     }
