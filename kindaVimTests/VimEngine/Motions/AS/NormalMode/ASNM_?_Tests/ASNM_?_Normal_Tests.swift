d6a117472bf5da22b6744df178376c3adb617d9d
Guill Lo <guill.bout@gmail.com>
2022-02-11 23:49:07 +0800

    update deps

--- kindaVimTests/VimEngine/Motions/AS/NormalMode/ASNM_?_Tests/ASNM_?_Normal_Tests.swift
+++ kindaVimTests/VimEngine/Motions/AS/NormalMode/ASNM_?_Tests/ASNM_?_Normal_Tests.swift
@@ -3,7 +3,7 @@ import KeyCombination
 import XCTest
 
 
-class SucceedingASNM_interrogationMark_Tests: ASNM_BaseTests {
+class SucceedingASNM_interrogationMark_Normal_Tests: ASNM_BaseTests {
     
     override func setUp() {
         super.setUp()
@@ -18,8 +18,7 @@ class SucceedingASNM_interrogationMark_Tests: ASNM_BaseTests {
 }
 
 
-// TODO: gonna have to make test with escape, delete etc
-extension SucceedingASNM_interrogationMark_Tests {
+extension SucceedingASNM_interrogationMark_Normal_Tests {
     
     func test_that_it_calls_the_correct_function_on_accessibility_strategy() {
         XCTAssertEqual(asNormalModeMock.functionCalled, "interrogationMark(times:to:on:)")
